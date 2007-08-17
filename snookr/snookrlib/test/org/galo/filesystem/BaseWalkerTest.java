3ce6a83dd2635d4d274eec6267bb8f5ffd12fa6d
Daniel Lauzon <daneroo@users.noreply.github.com>
2007-08-17 21:22:05 +0000

    MACAddress is broken on Vista

---
+++ snookr/snookrlib/test/org/galo/filesystem/BaseWalkerTest.java
@@ -0,0 +1,19 @@
+/* Copyright Notice
+ * This file contains proprietary information of Sologlobe Logistique Inc.
+ * Copying or reproduction without prior written approval is prohibited.
+ * Copyright (c) 1997-2005
+ */
+
+package org.galo.filesystem;
+
+import junit.framework.*;
+
+public class BaseWalkerTest extends TestCase  {
+
+
+  public void testInstaciate() {
+      BaseWalker walker = new BaseWalker(null,null);
+  }
+
+}
+
