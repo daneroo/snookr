fe8fb20b08bec7533df04bc7d53ec3fa17cce6a8
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2007-11-02 05:02:58 +0000

    new diff+upload

--- trunk/snookr/script4g/src/symmetricDiffs.groovy
+++ trunk/snookr/script4g/src/symmetricDiffs.groovy
@@ -5,6 +5,9 @@
 */
 
 import net.snookr.db.Database;
+import net.snookr.db.FSImageDAO;
+import net.snookr.db.FlickrImageDAO;
+import net.snookr.flickr.Photos;
 import net.snookr.synch.Filesystem2Database;
 import net.snookr.synch.Flickr2Database;
 
@@ -24,8 +27,45 @@ def flickr2db = new Flickr2Database();
 flickr2db.setDatabase(db);
 flickr2db.run();
 
-// now do some diffs. from db
-//fsImageDAO.getMapByPrimaryKey
+/*
+  Diffs algorithm
+   find all FSImages in db that are
+     -still present on disk
+     -not present in FlickrImage table.
+*/
+
+FSImageDAO fsImageDAO = new FSImageDAO();
+fsImageDAO.setDatabase(db);
+FlickrImageDAO flickrImageDAO = new FlickrImageDAO();
+flickrImageDAO.setDatabase(db);
+Photos photos = new Photos();
+
+Map dbMapByFileName = fsImageDAO.getMapByPrimaryKey();
+int total=0;
+int totalFound=0;
+int totalMissing=0;
+dbMapByFileName.each() { fileName,fsima -> //
+    //println("fn:${fileName} -> fsima:${fsima}");
+    //println "fn:${fileName} looking for md5:${fsima.md5}";
+    List found = flickrImageDAO.fetchForMD5(fsima.md5);
+    if (found && found.size()>0) {
+        totalFound++;
+    } else {
+        println "fn:${fileName} could not find md5:${fsima.md5}";
+        totalMissing++;
+
+        File f = new File(fileName);
+        if (f.exists()) {
+            int nuPhotoid = photos.uploadPhoto(f);
+            println "new photoid: ${nuPhotoid}";
+        } else {
+            println "Missing file not found in filesystem. cannot upload";
+        }
+
+    }
+    total++;
+}
+println "examined ${total} FSImages, found:${totalFound} missing:${totalMissing}";
 
 //println "-=-=-= Database Summary:  =-=-=-"
 //db.printSummary(false);
