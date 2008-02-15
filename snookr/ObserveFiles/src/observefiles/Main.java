b96729f95e2719d9763d664a6ee780058d14c99f
Daniel Lauzon <daneroo@users.noreply.github.com>
2008-02-15 06:03:41 +0000

    move to netimage, point to euler

--- snookr/ObserveFiles/src/observefiles/Main.java
+++ snookr/ObserveFiles/src/observefiles/Main.java
@@ -23,7 +23,7 @@ import javax.persistence.Persistence;
 import javax.persistence.Query;
 import net.snookr.filesystem.Filesystem;
 import net.snookr.model.FSImage;
-import net.snookr.model.Thing;
+import net.snookr.model.NetImage;
 import net.snookr.util.Exif;
 import net.snookr.util.MD5;
 import net.snookr.util.DateFormat;
@@ -45,8 +45,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("  Host: "+ HostInfo.getHostName());
-        // TODO code application logic here
+        String host = HostInfo.getHostName();
+        System.out.println("  Host: "+ host);
+        
         //String baseDirPath = "/home/daniel/media";
         String baseDirPath = "C:\\Users\\daniel\\Pictures";
         //String baseDirPath = "/home/daniel/media/Europe2002/5-Mirabel";
@@ -55,11 +56,11 @@ public class Main {
         }
         
         emf = Persistence.createEntityManagerFactory("ObserveFilesPU");
-        traverseFromBaseDir(baseDirPath);
-        validateStillExists();
+        traverseFromBaseDir(host,baseDirPath);
+        validateStillExists(host);
     }
     
-    private static void traverseFromBaseDir(final String baseDirPath) {
+    private static void traverseFromBaseDir(final String host,final String baseDirPath) {
         try {
             File baseDir = new File(baseDirPath).getCanonicalFile();
             
@@ -72,7 +73,7 @@ public class Main {
             //createTransactionalEntityManager();
             for (Object o : fsImageList) {
                 FSImage fsima = (FSImage)o;
-                createOrUpdate(fsima);
+                createOrUpdate(host,fsima);
             }
             //closeTransactionalEntityManager();
             
@@ -81,28 +82,13 @@ public class Main {
         }
     }
     
-    static Thing findThing(String fileName) {
-        return findThingByEach(fileName);
-        //return findThingByMap(fileName);
-    }
-    static Map<String,Thing> thingPredictorByFilename=null;
-    static Thing findThingByMap(String fileName) {
-        if (thingPredictorByFilename==null){
-            thingPredictorByFilename = new HashMap<String,Thing>();
-            Query queryFindAllThings = em.createNamedQuery("findAllThings");
-            List<Thing> listOfThings = queryFindAllThings.getResultList();
-            for (Thing t : listOfThings) {
-                thingPredictorByFilename.put(t.getFileName(),t);
-            }
-            System.out.println("Found "+listOfThings.size()+" things");
-        }
-        return thingPredictorByFilename.get(fileName);
-    }
-    static Thing findThingByEach(String fileName) {
-        Query queryFindThingByFileName = em.createNamedQuery("findThingByFileName");
-        queryFindThingByFileName.setParameter("fileName", fileName);
+    
+    static NetImage findNetImage(String host,String fileName) {
+        Query q = em.createNamedQuery("NetImage.findByHostAndFileName");
+        q.setParameter("host", host);
+        q.setParameter("fileName", fileName);
         try {
-            return (Thing)queryFindThingByFileName.getSingleResult();
+            return (NetImage)q.getSingleResult();
         } catch (EntityNotFoundException enfe) {
         } catch (NoResultException nre) {
         }
@@ -114,59 +100,59 @@ public class Main {
     static final int md5Always = 2;
     static final int md5Behaviour = md5AsNeeded; // include setter for behaviour
     
-    public static void createOrUpdate(FSImage fsima) {
+    public static void createOrUpdate(String host,FSImage fsima) {
         createTransactionalEntityManager();
         
-        Thing thing = new Thing(fsima);
-        Thing priorThing = findThing(thing.getFileName());
-        createOrUpdateInternal(thing,priorThing);
-        
-        if (priorThing!=null) {
-            //System.out.println("prior: "+priorThing);
+        NetImage nima = new NetImage(host,fsima);
+        NetImage priorNima = findNetImage(host,nima.getFileName());
+        if (priorNima!=null) {
+            //System.out.println("update: "+priorNima);
         } else {
-            System.out.println("new fsima: "+fsima);
+            System.out.println("create: "+nima);
         }
+        createOrUpdateInternal(nima,priorNima);
+        
         
         closeTransactionalEntityManager();
     }
     
-    private static String createOrUpdateInternal(Thing newThing,Thing predictorFromDB) {
+    private static String createOrUpdateInternal(NetImage newNima,NetImage predictorFromDB) {
         // implement parse (attr) and persist photo info from flickr
         boolean isNew = false;
         boolean isModified = false;
         
-        String fileName = newThing.getFileName();
+        String fileName = newNima.getFileName();
         File f = new File(fileName);
         
-        Thing persist = predictorFromDB;
+        NetImage persist = predictorFromDB;
         
         if (persist==null) {
-            persist = newThing;
+            persist = newNima;
             isNew = isModified = true;
         } else {
-            // attributes assumed to be set in newThing!
-            if (!newThing.getFileSize().equals( persist.getFileSize() ) ) {
-                persist.setFileSize(newThing.getFileSize());
+            // attributes assumed to be set in newNima!
+            if (!newNima.getFileSize().equals( persist.getFileSize() ) ) {
+                persist.setFileSize(newNima.getFileSize());
                 log("mod for filesize ");
                 isModified=true;
             }
-            if (! newThing.getLastModified().equals(  persist.getLastModified() ) ) {
+            if (! newNima.getLastModified().equals(  persist.getLastModified() ) ) {
                 // rouded milliseconds is ok: (Vista has mod times to the milli)
-                if (newThing.getLastModified().getTime()/1000l != persist.getLastModified().getTime()/1000l) {
-                    log("mod for lastmod: "+DateFormat.format(persist.getLastModified(),"????-??-?? ??:??:??")+" -> "+DateFormat.format(newThing.getLastModified(),"????-??-?? ??:??:??"));
-                    log("mod for lastmod ms: "+persist.getLastModified().getTime()+" -> "+newThing.getLastModified().getTime());
-                    persist.setLastModified( newThing.getLastModified() );
+                if (newNima.getLastModified().getTime()/1000l != persist.getLastModified().getTime()/1000l) {
+                    log("mod for lastmod: "+DateFormat.format(persist.getLastModified(),"????-??-?? ??:??:??")+" -> "+DateFormat.format(newNima.getLastModified(),"????-??-?? ??:??:??"));
+                    log("mod for lastmod ms: "+persist.getLastModified().getTime()+" -> "+newNima.getLastModified().getTime());
+                    persist.setLastModified( newNima.getLastModified() );
                     isModified = true;
                 } else {
-                    //log("close enough: "+persist.getLastModified().getTime()/1000l+" -> "+newThing.getLastModified().getTime()/1000l);
+                    //log("close enough: "+persist.getLastModified().getTime()/1000l+" -> "+newNima.getLastModified().getTime()/1000l);
                 }
             }
         }
         
-        // attributes not assumed to be set in newThing (because of cost...)
+        // attributes not assumed to be set in newNima (because of cost...)
         // TODO behaviour thing like md5: always/never/asNeeded
         if (persist.getTaken()==null) {
-            Date taken = newThing.getTaken();
+            Date taken = newNima.getTaken();
             if (taken==null) {
                 taken = Exif.getExifDate(f);
                 log( "extracted exif date "+DateFormat.format(taken,"????-??-?? ??:??:??")+" "+f.getName() );
@@ -181,7 +167,7 @@ public class Main {
         // TODO; OR IF isModified ????? redo md5sum,
         if (  (md5Behaviour!=md5Never) &&
                 (persist.getMd5() == null || md5Behaviour == md5Always) ) {
-            String md5 = newThing.getMd5();
+            String md5 = newNima.getMd5();
             if (md5==null || "".equals(md5) ) {
                 try {
                     md5 = MD5.digest(f);
@@ -211,11 +197,12 @@ public class Main {
         System.out.println(s);
     }
     
-    private static void validateStillExists() {
+    private static void validateStillExists(String host) {
         createEntityManager();
-        Query queryFindAllThings = em.createNamedQuery("findAllThings");
-        List<Thing> listOfThings = queryFindAllThings.getResultList();
-        for (Thing t : listOfThings) {
+        Query q = em.createNamedQuery("NetImage.findByHost");
+        q.setParameter("host",host);
+        List<NetImage> listOfNimas = q.getResultList();
+        for (NetImage t : listOfNimas) {
             File f = new File(t.getFileName());
             if (!f.exists()) {
                 System.out.println("Could not find "+f);
