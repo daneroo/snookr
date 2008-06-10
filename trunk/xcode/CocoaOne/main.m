2acf6c28311e9ed399f9f9bdcc2947e2f97013b1
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2008-06-10 18:09:26 +0000

    CocoaOne start

---
+++ trunk/xcode/CocoaOne/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CocoaOne
+//
+//  Created by Daniel Lauzon on 09/06/08.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
