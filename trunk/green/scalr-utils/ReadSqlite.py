28ab7dd27927615474805033c6ed51b39b4241a4
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2008-08-01 19:00:58 +0000

    starting time arith on TED sqlite data

--- trunk/green/scalr-utils/ReadSqlite.py
+++ trunk/green/scalr-utils/ReadSqlite.py
@@ -13,8 +13,14 @@ else:
 if not os.path.exists(filename):
 	print "Could not find SQLite3 db file: %s" % filename
 	sys.exit(0);
+# time formatting:
+# 1 second between readings
+#  print 633530465840008874 - 633530465830000554
+# --> 10008320
+# 90000 seconds
+# print (633530465840008874 - 633529550800006250 ) / 10000000
 
-conn = sqlite3.connect(sys.argv[1])
+conn = sqlite3.connect(filename)
 c = conn.cursor()
 c.execute('select * from rdu_second_data')
 for row in c:
