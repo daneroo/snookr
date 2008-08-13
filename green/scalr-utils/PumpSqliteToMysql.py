259ccbf697cfaa8cfcdbcf6ee0153e5ce7193b38
Daniel Lauzon <daneroo@users.noreply.github.com>
2008-08-13 00:59:42 +0000

    refactor and prepare incremental load

--- green/scalr-utils/PumpSqliteToMysql.py
+++ green/scalr-utils/PumpSqliteToMysql.py
@@ -10,8 +10,30 @@ def cnvTime(tedTimeString):
 	secs = ( string.atol(tedTimeString) / 10000 - 62135582400000 ) / 1000
 	return time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(secs))
 
+def invTime(secs):
+        tedTimeLong = long( ((secs * 1000) +  62135582400000)*10000 ) 
+        return "%019ld" % tedTimeLong
 
-#print "We have  ",len(sys.argv) ," arguments"
+def testTime():
+        secs = time.time()
+        log("Testing time from secs:%d" % secs)
+        stamp = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(secs))
+        ted = invTime(secs)
+        log("stamp=%s invTime=%s" % ( stamp, ted ) )
+        log("ted=  %s cnvTime=%s" % ( ted, cnvTime(ted) ) )
+        sys.exit(0)
+        
+def log(msg):
+	stamp = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())        
+        sys.stderr.write("%s %s\n" % (stamp,msg));
+
+def getScalar(sql):
+        curs = connsqlite.cursor()
+        curs.execute(sql)
+        row = curs.fetchone()
+        curs.close()
+        return row[0]
+
 if len(sys.argv) < 2:
 	print "usage : %s <SQLite3 db file>" % sys.argv[0]
 	sys.exit(0);
@@ -28,23 +50,41 @@ if not os.path.exists(filename):
 # 90000 seconds
 # print (633530465840008874 - 633529550800006250 ) / 10000000
 
+
+log("Pump started")
 countRows=0
 connsqlite = sqlite.connect(filename)
+
+#log("count: %s" % getScalar('select count(*) from rdu_second_data'))
+#log("max: %s" % getScalar('select max(tick) from rdu_second_data'))
+
 curssqlite = connsqlite.cursor()
-curssqlite.execute('select * from rdu_second_data')
+curssqlite.execute('select tick,kw from rdu_second_data')
+
+#curssqlite.execute('select tick,kw from rdu_second_data order by tick desc limit 100000')
+#curssqlite.execute('select tick,kw from rdu_second_data where tick>"0633540625260005000"')
+
+#hourAgo = invTime(time.time()-10*60)
+#hourAgo = invTime(time.time()+100)
+#curssqlite.execute('select tick,kw from rdu_second_data where tick>"%s"' % hourAgo)
+
+log("SQL executed");
 for row in curssqlite:
         stamp = cnvTime(row[0])
         watt = row[1]*1000
         countRows+=1
         if (countRows%20000 == 0):
-                sys.stderr.write("rows: %d stamp: %s\n" % (countRows,stamp));
+                log("rows: %8d stamp: %s [%s]" % (countRows,stamp,row[0]));
                 
         # print cnvTime(row[0]), row[1]*1000
         # could use REPLACE INTO, instead, or ON DUPLICATE update count=count+1.
-        print ("INSERT IGNORE INTO watt (stamp, watt) VALUES ('%s', '%d');" % (stamp,watt))
+        #print ("INSERT IGNORE INTO watt (stamp, watt) VALUES ('%s', '%d');" % (stamp,watt))
+        print ("REPLACE INTO watt (stamp, watt) VALUES ('%s', '%d');" % (stamp,watt))
 
 curssqlite.close()
 connsqlite.close()
+
+log("Done");
 
 
 #connmysql = MySQLdb.connect (host = "127.0.0.1",
