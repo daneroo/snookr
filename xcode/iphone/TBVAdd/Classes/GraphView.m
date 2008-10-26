1b6a8f830754d6c2fe7483094e80870b6559b10c
Daniel Lauzon <daneroo@users.noreply.github.com>
2008-10-26 07:25:12 +0000

    added daysAgo for use in different view Scope, gradient based background, begining of axis draw.

--- xcode/iphone/TBVAdd/Classes/GraphView.m
+++ xcode/iphone/TBVAdd/Classes/GraphView.m
@@ -20,12 +20,30 @@
         self.autoresizingMask = UIViewAutoresizingFlexibleWidth;
         self.contentMode = UIViewContentModeRedraw;
         
-        startVal = 189.2 * 1000;
+        startVal = 190.0 * 1000;
         goalVal = 169.0 * 1000;
+		daysAgo = 7;
     }
     return self;
 }
 
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+	switch (daysAgo) {
+		case 7:
+			daysAgo=30;
+			break;
+		case 30:
+			daysAgo=60;
+			break;
+		case 60:
+			daysAgo=365;
+			break;
+		default:
+			daysAgo=7;
+	}
+	NSLog(@"touch ended Event  daysAgo: %d",daysAgo);
+	[self setNeedsDisplay];
+}
 
 -(void)reportRange {
     NSDate *minDate = [NSDate dateWithTimeIntervalSince1970:minTime];
@@ -34,11 +52,14 @@
 }
 
 -(void)findRange {
+	NSDate *ago = [NSDate dateWithTimeIntervalSinceNow:(-daysAgo*24*3600)];
+
     NSEnumerator * enumerator = [observations objectEnumerator];
     Observation *observation;
     NSDate *minDate=nil, *maxDate=nil;
     CGFloat localMinVal=100000000.0, localMaxVal=0.0;
     while(observation = (Observation *)[enumerator nextObject]) {
+		if ([observation.stamp compare:ago]<0) continue;
         //NSLog(@"-- stamp: %@, value: %d", observation.stamp, observation.value);
         if (!minDate || [minDate compare:observation.stamp]>0) {
             minDate = observation.stamp;
@@ -54,26 +75,63 @@
         }
     }
     // include goal in range:
-    if (localMinVal>goalVal) {
-        localMinVal = goalVal;
-    }
-    if (localMaxVal<goalVal) {
-        localMaxVal = goalVal;
-    }
+    if (localMinVal>goalVal) localMinVal = goalVal;
+    if (localMaxVal<goalVal) localMaxVal = goalVal;
     // include startValue in range:
-    if (localMinVal>startVal) {
-        localMinVal = startVal;
-    }
-    if (localMaxVal<startVal) {
-        localMaxVal = startVal;
-    }
+    //if (localMinVal>startVal) localMinVal = startVal;
+    //if (localMaxVal<startVal) localMaxVal = startVal;
     
+	if ([minDate compare:ago]<0) {
+		minDate = ago;
+	}
+	
     minTime = [minDate timeIntervalSince1970];
     maxTime = [maxDate timeIntervalSince1970];
     minVal = localMinVal;
     maxVal = localMaxVal;
     [self reportRange];
     
+}
+
+- (CGFloat) mapX:(NSTimeInterval) obsx {
+	double xRange = maxTime - minTime;
+	return 20+(self.bounds.size.width-40)*(obsx-minTime)/xRange;
+}
+
+- (CGFloat) mapY:(CGFloat) obsy {
+	double yRange = maxVal - minVal;
+	return (self.bounds.size.height-20)-(self.bounds.size.height-40)*(obsy-minVal)/yRange;
+}
+
+- (void)fillWithGradient:(CGContextRef)context {
+	CGColorSpaceRef rgb = CGColorSpaceCreateDeviceRGB();
+	CGFloat colors[] = {
+		0,0,0,1,
+		0,1.0/3.0,0,1,
+		/*
+		0,1.0/3.0,0,1,
+		0,1.0/3.0,0,1,
+		0,1.0/3.0,0,1,
+		0,1.0/3.0,0,1,
+		0,1.0/3.0,0,1,
+		1,1,1,1,
+		 */
+	};
+	CGGradientRef gradient;
+	gradient = CGGradientCreateWithColorComponents(rgb, colors, NULL, sizeof(colors)/(sizeof(colors[0])*4));
+	CGColorSpaceRelease(rgb);
+
+	CGContextSaveGState(context);
+	//CGContextClipToRect(context, clips[0]);
+	
+	CGRect b = self.bounds;
+	CGPoint start = CGPointMake(b.origin.x, b.origin.y + b.size.height * 0.0);
+	CGPoint end = CGPointMake(b.origin.x, b.origin.y + b.size.height * 0.5);
+	CGContextDrawLinearGradient(context, gradient, start, end, 0);
+	  //not needed here kCGGradientDrawsBeforeStartLocation | kCGGradientDrawsAfterEndLocation
+	CGContextRestoreGState(context);
+	
+	CGGradientRelease(gradient);
 }
 
 - (void)drawRect:(CGRect)rect {
@@ -84,53 +142,100 @@
     CGContextRef context = UIGraphicsGetCurrentContext();
     //CGRect bounds = self.bounds;
     
-    // Drawing code
-    // Drawing lines with a white stroke color
+	[self fillWithGradient:context];
+
 	CGContextSetRGBStrokeColor(context, 1.0, 1.0, 1.0, 1.0);
-	// Draw them with a 2.0 stroke width so they are a bit more visible.
 	
-        double xRange = maxTime - minTime;
-        double yRange = maxVal - minVal;
+	CGFloat xleft = [self mapX:minTime]; // maybe less and clip
+	CGFloat xright = [self mapX:maxTime]; // maybe more and clip
+	CGContextSetLineWidth(context, 1.0);
 
-        // drawing goes from
-        // origin         : 10.0,self.bounds.size.height-10 
-        // to upper right : self.bounds.size.width-10, 10
-        
-        // Draw a single line from left to right
-        CGFloat ygoal =  (self.bounds.size.height-10)-(self.bounds.size.height-20)*(goalVal-minVal)/yRange;
-        CGFloat ystart =  (self.bounds.size.height-10)-(self.bounds.size.height-20)*(startVal-minVal)/yRange;
+	// Axis yT + Ticks
+	CGFloat yForXaxis = [self mapY:minVal]+3;
 
-        CGContextSetLineWidth(context, 1.0);
-        CGFloat dash[] = {5.0, 5.0};
-        CGContextSetLineDash(context, 0.0, dash, 2);
-        CGContextMoveToPoint(context, 10.0, ystart);
-        CGContextAddLineToPoint(context, self.bounds.size.width-10, ystart);
-        CGContextMoveToPoint(context, 10.0, ygoal);
-        CGContextAddLineToPoint(context, self.bounds.size.width-10, ygoal);
-        CGContextStrokePath(context);
-        CGContextSetLineDash(context, 0.0, NULL, 0);
-        
-        
-        CGContextSetLineWidth(context, 1.0);
-        // Draw a connected sequence of line segments
-        int numberofitems = [observations count];
-        CGPoint pointarray[numberofitems];
-        
-        for (int i=0;i<numberofitems;i++) {
-            Observation *observation = (Observation *)[observations objectAtIndex:i];
-            NSTimeInterval obsx = [observation.stamp timeIntervalSince1970];
-            CGFloat x = 10+(self.bounds.size.width-20)*(obsx-minTime)/xRange;
+	CGContextMoveToPoint(context, xleft, yForXaxis);
+	CGContextAddLineToPoint(context, xright, yForXaxis);
+	for (int i=0;i<5;i++) {
+		CGFloat xTick = xleft+(xright-xleft)*i/4.0;
+		CGContextMoveToPoint(context, xTick, yForXaxis-0);
+		CGContextAddLineToPoint(context, xTick, yForXaxis+3);
+	}
 
-            CGFloat obsy = observation.value;
-            CGFloat y = (self.bounds.size.height-10)-(self.bounds.size.height-20)*(obsy-minVal)/yRange;
-            NSLog(@"p = %f,%f",x,y);
-            
-            pointarray[i] = CGPointMake(x,y);
-        }
-        // Bulk call to add lines to the current path.
-        // Equivalent to MoveToPoint(points[0]); for(i=1; i<count; ++i) AddLineToPoint(points[i]);
-        CGContextAddLines(context, pointarray, sizeof(pointarray)/sizeof(pointarray[0]));
-        CGContextStrokePath(context);
+	CGFloat xForYaxis = [self mapX:minTime]-5;
+	CGFloat ybot = [self mapY:minVal]; // maybe less and clip
+	CGFloat ytop = [self mapY:maxVal]; // maybe more and clip
+	CGContextMoveToPoint(context, xForYaxis,ybot);
+	CGContextAddLineToPoint(context, xForYaxis,ytop);
+	for (int i=0;i<5;i++) {
+		CGFloat yTick = ybot-(ybot-ytop)*i/4.0;
+		CGContextMoveToPoint(context, xForYaxis-0, yTick);
+		CGContextAddLineToPoint(context, xForYaxis-3, yTick);
+	}
+	// actually draw both axis+ticks
+	//[[UIColor whiteColor] set];
+	[[UIColor lightGrayColor] set];
+	CGContextStrokePath(context);
+	
+	[[UIColor darkGrayColor] set];
+	NSString *text=[NSString stringWithFormat:@"%d Days",daysAgo];
+	CGFloat fontSize = [UIFont smallSystemFontSize]; //=12
+	UIFont *font = [UIFont systemFontOfSize:fontSize];
+	CGPoint point = CGPointMake(
+								[self mapX:maxTime] - [text sizeWithFont:font].width,
+								[self mapY:maxVal]-15);
+	[text drawAtPoint:point withFont:font];
+	[[UIColor lightGrayColor] set];
+	for (int i=0;i<5;i++) {
+		CGFloat yTick = ybot-(ybot-ytop)*i/4.0;
+		NSString *tickText=[NSString stringWithFormat:@"%d",i];
+		CGSize tsz=[tickText sizeWithFont:font];
+		CGPoint point = CGPointMake(xForYaxis-tsz.width-3,yTick-tsz.height/2.0);
+		[tickText drawAtPoint:point withFont:font];
+
+		CGFloat xTick = xleft+(xright-xleft)*i/4.0;
+		point = CGPointMake(xTick-tsz.width/2.0, yForXaxis+3);
+		[tickText drawAtPoint:point withFont:font];
+
+	}
+	
+	
+	// Draw y=Konstant lines from left to right
+	CGFloat ystart = [self mapY:startVal];
+	CGFloat ygoal =  [self mapY:goalVal];
+	
+	CGFloat dash[] = {5.0, 5.0};
+	CGContextSetLineDash(context, 0.0, dash, 2);
+	CGContextSetRGBStrokeColor(context, .5,.5,.5,1);
+	CGContextMoveToPoint(context, xleft, ystart);
+	CGContextAddLineToPoint(context, xright, ystart);
+	CGContextStrokePath(context);
+	
+	CGContextSetRGBStrokeColor(context, 0.0, 1.0, 0.0, 1.0);
+	CGContextMoveToPoint(context, xleft, ygoal);
+	CGContextAddLineToPoint(context, xright, ygoal);
+	CGContextStrokePath(context);
+
+	CGContextSetLineDash(context, 0.0, NULL, 0);
+	CGContextSetRGBStrokeColor(context, 1.0, 1.0, 1.0, 1.0);
+	
+	CGContextSetLineWidth(context, 2.0);
+	// Draw a connected sequence of line segments
+	int numberofitems = [observations count];
+	CGPoint pointarray[numberofitems];
+	
+	
+	for (int i=0;i<numberofitems;i++) {
+		Observation *observation = (Observation *)[observations objectAtIndex:i];
+		NSTimeInterval obsx = [observation.stamp timeIntervalSince1970];
+		pointarray[i] = CGPointMake([self mapX:obsx],[self mapY:observation.value]);
+	}
+	CGContextSaveGState(context);
+	CGContextClipToRect(context, CGRectMake(20, 20, self.bounds.size.width-40, self.bounds.size.height-40));
+	// Bulk call to add lines to the current path.
+	// Equivalent to MoveToPoint(points[0]); for(i=1; i<count; ++i) AddLineToPoint(points[i]);
+	CGContextAddLines(context, pointarray, sizeof(pointarray)/sizeof(pointarray[0]));
+	CGContextStrokePath(context);
+	CGContextRestoreGState(context);
 }
 
 
