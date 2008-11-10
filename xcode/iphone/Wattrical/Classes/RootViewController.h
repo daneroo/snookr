6b75d280013056be0a426db85112a3dc7a1beb44
Daniel Lauzon <daneroo@users.noreply.github.com>
2008-11-10 20:26:33 +0000

    Wattrical initial import

---
+++ xcode/iphone/Wattrical/Classes/RootViewController.h
@@ -0,0 +1,14 @@
+//
+//  RootViewController.h
+//  Wattrical
+//
+//  Created by Daniel Lauzon on 10/11/08.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+}
+
+@end
