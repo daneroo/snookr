6a0b3dd5fca7ebe57d45e2e0aa1c973788afbfdf
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2008-11-11 15:15:16 +0000

    keep php files

---
+++ trunk/green/scalr-utils/php/getTED.php
@@ -0,0 +1,26 @@
+<?php
+header("Content-type: text/plain");
+
+
+$url = 'http://aria.dl.sologlobe.com:9090/DashboardData';
+$content = file_get_contents($url);
+// first method
+$xml = new SimpleXMLElement($content);
+
+$kwnow =  (string)$xml->KWNow; // explicit string (not element)
+$watt = 1000.0 * $kwnow;
+
+//$nowstr = date('c');  // alomost: 2008-11-11T02:15:11-05:00
+// we want: 2008-11-11T10:12:19Z-0500
+$nowstr = date('Y-m-d').'T'.date('G:i:s').'Z'.date('O');
+
+
+
+$xmldecl = "<?xml version=\"1.0\"?>\n"; 
+$xmldecl .= "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n";
+echo $xmldecl;
+
+echo "<plist version=\"1.0\">\n";
+echo "<dict><key>stamp</key><date>$nowstr</date><key>value</key><integer>$watt</integer></dict>\n";
+echo "</plist>";
+?>
