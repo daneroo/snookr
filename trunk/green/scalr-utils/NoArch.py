76a4bd76057971967868f481254c3f52a6c76d34
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2008-11-14 18:26:48 +0000

    created NoArch module test

--- trunk/green/scalr-utils/NoArch.py
+++ trunk/green/scalr-utils/NoArch.py
@@ -1,52 +1,55 @@
 import sys
 import os
-# This also works!
-#try:
-#	from pysqlite2 import dbapi2 as sqlite
-#except:
-#	print "import failed trying sqlite3"
-#	import sqlite3 as sqlite
+#import string
+#import time
 
-import string
-import time
-
+def logError(msg):
+	sys.stderr.write(msg)
+	sys.stderr.write("\n")
+def logWarn(msg):
+	logError(msg)
+def logInfo(msg):
+	logError(msg)
+	
 def SqliteConnectNoArch(filename): # return either a sqlite3/2/1 connection
 	if not os.path.exists(filename):
-		print "Could not find SQLite3 db file: %s" % filename
+		logError("Could not find SQLite3 db file: %s" % filename)
 		sys.exit(0);
 	try:
 		from pysqlite2 import dbapi2 as sqlite
-		print 'Using sqlite-2'
+		logInfo('Using sqlite-2')
 		return sqlite.connect(filename)
 	except:
 		pass
-#		print "from pysqlite2 import dbapi2 failed"
+#		logWarn("from pysqlite2 import dbapi2 failed")
 	try:
 		import sqlite3 as sqlite;
-		print 'Using sqlite-3'
+		logInfo('Using sqlite-3')
 		return sqlite.connect(filename)
 	except:
 		pass
-#		print "import sqlite3 failed"
+#		logWarn("import sqlite3 failed")
 	try:
 		import sqlite
-		print 'Using sqlite-1'
+		logInfo('Using sqlite-1')
 		return sqlite.connect(filename)
 	except:
 		pass
-#		print "import sqlite failed"
+#		logWarn("import sqlite failed")
 	return None
 	
+if __name__ == "__main__":
+	
+	import sys
+	import os
+	if len(sys.argv) < 2:
+		print "usage : %s <SQLite3 db file>" % sys.argv[0]
+		sys.exit(0);
+	else:
+		filename = sys.argv[1]
 
-if len(sys.argv) < 2:
-	print "usage : %s <SQLite3 db file>" % sys.argv[0]
-	sys.exit(0);
-else:
-	filename = sys.argv[1]
-
-
-conn = SqliteConnectNoArch(filename)
-c = conn.cursor()
-c.execute('select * from rdu_second_data limit 10')
-for row in c:
-	print "| %s | %f | %f | %f |" % row
+	conn = SqliteConnectNoArch(filename)
+	c = conn.cursor()
+	c.execute('select * from rdu_second_data limit 10')
+	for row in c:
+		print "| %s | %f | %f | %f |" % row
