dec12b00e02bc0fa4ecd13772b68fc35cab7f473
Daniel Lauzon <daneroo@users.noreply.github.com>
2008-11-14 20:25:15 +0000

    Refactored Incrementa

--- green/scalr-utils/Incremental.py
+++ green/scalr-utils/Incremental.py
@@ -1,92 +1,101 @@
 
 import sys
 import os
-#import sqlite
-from pysqlite2 import dbapi2 as sqlite
 import string
 import time
-import MySQLdb
+import getopt
+import scalr
+# for shortcuts, or even from scalr import *
+from scalr import logInfo,logWarn,logError
+from scalr import cnvTime,invTime,testTime
+from scalr import getScalar
 
-def cnvTime(tedTimeString):
-	secs = ( string.atol(tedTimeString) / 10000 - 62135582400000 ) / 1000
-	return time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(secs))
+#def main():
+usage = 'python %s --db <dbfile> (--all | --secs <secs> --minutes <minutes> --hours <hours> --days <days>)' % sys.argv[0]
 
-def invTime(secs):
-        tedTimeLong = long( ((secs * 1000) +  62135582400000)*10000 ) 
-        return "%019ld" % tedTimeLong
+# parse command line options
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "", ["db=", "all", "secs=","minutes=","hours=","days="])
+except getopt.error, msg:
+	logError('error msg: %s' % msg)
+	logError(usage)
+	sys.exit(2)
 
-def testTime():
-        secs = time.time()
-        log("Testing time from secs:%d" % secs)
-        stamp = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(secs))
-        ted = invTime(secs)
-        log("stamp=%s invTime=%s" % ( stamp, ted ) )
-        log("ted=  %s cnvTime=%s" % ( ted, cnvTime(ted) ) )
-        sys.exit(0)
-        
-def log(msg):
-	stamp = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())        
-        sys.stderr.write("%s %s\n" % (stamp,msg));
-
-def getScalar(sql):
-        curs = connsqlite.cursor()
-        curs.execute(sql)
-        row = curs.fetchone()
-        curs.close()
-        return row[0]
-
-if len(sys.argv) < 2:
-	print "usage : %s <SQLite3 db file>" % sys.argv[0]
-	sys.exit(0);
-else:
-	filename = sys.argv[1]
-
-if not os.path.exists(filename):
-	print "Could not find SQLite3 db file: %s" % filename
-	sys.exit(0);
-# time formatting:
-# 1 second between readings
-#  print 633530465840008874 - 633530465830000554
-# --> 10008320
-# 90000 seconds
-# print (633530465840008874 - 633529550800006250 ) / 10000000
-
-
-log("Incremental Pump started")
+dbfilename = ''
 daysAgo=0
 hoursAgo=0
-minutesAgo=5
+minutesAgo=0
 secondsAgo=0
+allTime = False
 
-referenceTimeAsTed = invTime(time.time()-((daysAgo*24+hoursAgo)*60+minutesAgo)*60+secondsAgo)
-log("referenceTime: %s - %s" % (referenceTimeAsTed, cnvTime(referenceTimeAsTed)) )
+for o, a in opts:
+	if o == "--db":
+		dbfilename = a
+	elif o == "--secs":
+		secondsAgo = string.atol(a)
+	elif o == "--all":
+		allTime = True
+	elif o == "--minutes":
+		minutesAgo = string.atol(a)
+	elif o == "--hours":
+		hoursAgo = string.atol(a)
+	elif o == "--days":
+		daysAgo = string.atol(a)
 
-# include cnvTime in select:
+totalSecondsAgo = ((daysAgo*24+hoursAgo)*60+minutesAgo)*60+secondsAgo
+
+if dbfilename == '':
+	logError('specify --db dbfile:')
+	logError(usage)
+	sys.exit(2)
+
+if not os.path.exists(dbfilename):
+	logError("Could not find SQLite3 db file: %s" % dbfilename)
+	sys.exit(2);
+
+if not allTime and totalSecondsAgo == 0:
+	logError(usage)
+	logError('Use --all or at least one of --secs --minutes --hours --days')
+	sys.exit(2)
+
+
+logInfo("Incremental Pump started")
+
 fullSql = 'select tick,kw from rdu_second_data'
-incrementalSql = "select tick, kw from rdu_second_data where tick>='%s'" % referenceTimeAsTed
+sql = fullSql
+if not allTime:
+	referenceTimeAsTed = invTime(time.time()-totalSecondsAgo)
+	logInfo("referenceTime: %s - %s" % (referenceTimeAsTed, cnvTime(referenceTimeAsTed)) )
+	# include cnvTime in select:
+	incrementalSql = "%s where tick>='%s'" % (fullSql,referenceTimeAsTed)
+	#convertedIncrementalSql = "select datetime((tick/10000-62135582400000)/1000,\"unixepoch\",\"localtime\"), kw from rdu_second_data where tick>='%s'" % referenceTimeAsTed
+	#logInfo("sql: %s" % incrementalSql)
+	#logInfo("sql: %s" % convertedIncrementalSql)
+	sql = incrementalSql
+else:
+	logInfo("Full Dump (--all) : %d" % allTime)
+	#logInfo("sql: %s" % fullSql)
 
-convertedIncrementalSql = "select datetime((tick/10000-62135582400000)/1000,\"unixepoch\",\"localtime\"), kw from rdu_second_data where tick>='%s'" % referenceTimeAsTed
+logInfo("using sql: %s" % sql)
 
-#log("sql: %s" % fullSql)
-#log("sql: %s" % incrementalSql)
-#log("sql: %s" % convertedIncrementalSql)
+sys.exit(0)
+connsqlite = scalr.SqliteConnectNoArch(dbfilename)
 
-countRows=0
-connsqlite = sqlite.connect(filename)
 
-#log("count: %s" % getScalar('select count(*) from rdu_second_data'))
-#log("max: %s" % getScalar('select max(tick) from rdu_second_data'))
+#logInfo("count: %s" % getScalar('select count(*) from rdu_second_data'))
+#logInfo("max: %s" % getScalar('select max(tick) from rdu_second_data'))
 
 curssqlite = connsqlite.cursor()
 curssqlite.execute(incrementalSql)
 
-log("SQL executed");
+logInfo("SQL executed");
+countRows=0
 for row in curssqlite:
         stamp = cnvTime(row[0])
         watt = row[1]*1000
         countRows+=1
         if (countRows%10000 == 1):
-                log("rows: %8d stamp: %s [%s]" % (countRows,stamp,row[0]));
+                logInfo("rows: %8d stamp: %s [%s]" % (countRows,stamp,row[0]));
                 
         # print cnvTime(row[0]), row[1]*1000
         # could use REPLACE INTO, instead, or ON DUPLICATE update count=count+1.
@@ -96,5 +105,18 @@ for row in curssqlite:
 curssqlite.close()
 connsqlite.close()
 
-log("Done (%d total rows)" % countRows);
+logInfo("Done (%d total rows)" % countRows);
 
+
+# import MySQLdb
+#connmysql = MySQLdb.connect (host = "127.0.0.1",
+#                             user = "aviso",
+#                             passwd = "",
+#                             db = "ted")
+#cursmysql = connmysql.cursor()
+#cursmysql.execute("INSERT IGNORE INTO watt (stamp, watt) VALUES ('%s', '%d')" % (stamp,watt))
+#cursmysql.close()
+#connmysql.close()
+
+#if __name__ == '__main__':
+#  main()
