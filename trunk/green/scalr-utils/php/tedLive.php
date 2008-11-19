9e7a9e7f204f8139282d08c0488dc787204fcf44
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2008-11-19 18:58:42 +0000

    ted live now has scopes

--- trunk/green/scalr-utils/php/tedLive.php
+++ trunk/green/scalr-utils/php/tedLive.php
@@ -14,7 +14,35 @@ $xmldecl .= "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www
 echo $xmldecl;
 
 echo "<plist version=\"1.0\"><array>\n";
-$query = 'SELECT unix_timestamp(stamp),watt FROM tedlive order by stamp desc limit 100';
+# old live query
+#$query = 'SELECT unix_timestamp(stamp),watt FROM tedlive order by stamp desc limit 30';
+//print "<!-- Scope param : ".htmlspecialchars($_GET["scope"])."  --> \n"; 
+$scope=0;
+$scope=intval(htmlspecialchars($_GET["scope"]));
+if ($scope>3) $scope = fmod($scope,4);
+if ($scope<0) $scope = fmod(intval(time()/10),4);
+//print "<!-- Scope value : $scope  --> \n"; 
+
+if ($scope == 0) {
+    $len = 18;   $rightpad = '0';           $secondsPerSample=10;    $samples=30;  // tensec
+ } elseif ($scope == 1) {
+     $len = 16;   $rightpad = ':00';         $secondsPerSample=60;    $samples=60;  // minute
+ } elseif ($scope == 2) {
+     $len = 13;   $rightpad = ':00:00';      $secondsPerSample=3600;  $samples=24;  // hour
+ } elseif ($scope == 3) {
+     $len = 10;   $rightpad = ' 00:00:00';   $secondsPerSample=86400; $samples=31;  // day
+ }
+
+$secondsInWhere = $secondsPerSample * (1+$samples);
+$query = "select ";
+$query .= " unix_timestamp(str_to_date(concat(left(stamp,$len),'$rightpad'),'%Y-%m-%d %H:%i:%s')) as g, round(avg(watt)) ";
+$query .= " from tedlive ";
+$query .= " where stamp>date_sub(now(), interval $secondsInWhere second) ";
+$query .= " group by g  order by g desc";
+$query .= " limit $samples";
+
+//print "<!-- Query: ".$query."  --> \n"; 
+
 $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
 while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
