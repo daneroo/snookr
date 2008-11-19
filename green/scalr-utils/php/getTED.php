14b9e97f42200795bc0b25a58cf4dd7eb39a201e
Daniel Lauzon <daneroo@users.noreply.github.com>
2008-11-19 19:20:42 +0000

    update what is deployes

--- green/scalr-utils/php/getTED.php
+++ green/scalr-utils/php/getTED.php
@@ -12,7 +12,8 @@ $watt = 1000.0 * $kwnow;
 
 //$nowstr = date('c');  // alomost: 2008-11-11T02:15:11-05:00
 // we want: 2008-11-11T10:12:19Z-0500
-$nowstr = date('Y-m-d').'T'.date('G:i:s').'Z'.date('O');
+//$nowstr = date('Y-m-d').'T'.date('G:i:s').'Z'.date('O');
+$nowstr = date('Y-m-d').'T'.date('G:i:s').'Z';
 
 
 
@@ -20,7 +21,7 @@ $xmldecl = "<?xml version=\"1.0\"?>\n";
 $xmldecl .= "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n";
 echo $xmldecl;
 
-echo "<plist version=\"1.0\">\n";
+echo "<plist version=\"1.0\"><array>\n";
 echo "<dict><key>stamp</key><date>$nowstr</date><key>value</key><integer>$watt</integer></dict>\n";
-echo "</plist>";
+echo "</array></plist>";
 ?>
