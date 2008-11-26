769b3b4cc8ec972e47a586320f3ebe12c7eae400
Daniel Lauzon <daneroo@users.noreply.github.com>
2008-11-26 16:47:28 +0000

    created dayGenerator for Summarize

--- green/scalr-utils/Summarize.py
+++ green/scalr-utils/Summarize.py
@@ -1,42 +1,43 @@
 import MySQLdb
 import sys
+import time
+from scalr import logInfo,logWarn,logError
 
-print "Hello MySQL"
 
-conn = MySQLdb.connect (host = "127.0.0.1",
-                        user = "aviso",
-                        passwd = "",
-                        db = "ted")
-cursor = conn.cursor ()
+# add main, usage
+# create if necessary
+# args --drop --start,... --mysqldb --update (select >current (+ offset))
+# walk up the dates for day-month, or find some clever mysql dst expressions
 
 def dropAndCreateTable(name):
-    print "making %s" % (name)
-    tablename="watt%s" % name
-    ddl = """
-CREATE TABLE %s (
+    tablename="watt_%s" % name
+    logInfo("Making table for %s (%s)" % (name,tablename))
+    ddl = """CREATE TABLE %s (
 stamp datetime NOT NULL default '1970-01-01 00:00:00',
 watt int(11) NOT NULL default '0',
-PRIMARY KEY wattByStamp (stamp)
+PRIMARY KEY %sByStamp (stamp)
 );
-""" % (tablename)
+""" % (tablename,tablename)
     cursor.execute ("DROP TABLE IF EXISTS %s" % tablename)
     cursor.execute(ddl)
-    print "ddl executed for %s " % (tablename)
+    logInfo(" ddl executed for %s " % (tablename))
+    #print ddl
 
 def dropAndCreateTables():
-    dropAndCreateTable("tensec");
-    dropAndCreateTable("minute");
-    dropAndCreateTable("hour");
-    dropAndCreateTable("day");
+    suffixes = ["tensec","minute","hour","day","month"]
+    for suffix in suffixes:
+        dropAndCreateTable(suffix);
+    logInfo("Done creating Tables")
 
-def fillTable(name,groupingWidth,rightPad):
+def fillTable(suffix,groupingWidth,rightPad):
     # example for watttensec - why we have rightPad
     # replace into watttensec select concat(left(stamp,18),'0') as g,avg(watt) from watt where stamp>'2008-09-15 11:00:00' group by g;
     
     # example : cursor.execute("replace into wattminute select left(stamp,16) as g,avg(watt) from watt group by g")
-    tablename="watt%s" % name
+    tablename="watt_%s" % suffix
+    now = time.time()
     cursor.execute("replace into %s select concat(left(stamp,%d),'%s') as g,avg(watt) from watt group by g" % (tablename,groupingWidth,rightPad))
-    print "Table %s now has %d entries" % (tablename,getScalar("select count(*) from %s" % tablename))
+    print "Table %s now has %d entries (%6.2fs.)" % (tablename,getScalar("select count(*) from %s" % tablename),time.time()-now)
 
     
 def fillTables():
@@ -44,17 +45,64 @@ def fillTables():
     fillTable("minute",16,':00')
     fillTable("hour",13,':00:00')
     fillTable("day",10,' 00:00:00')
+    fillTable("month",7,'-01 00:00:00')
 
 def getScalar(sql):
     cursor.execute(sql)
     row = cursor.fetchone()
     return row[0]
 
-#dropAndCreateTables()
-fillTables()
+def startOfDay(secs,offsetInDays):
+    secsTuple = time.localtime(secs)
+    startOfDayWithOffsetTuple = (secsTuple[0],secsTuple[1],secsTuple[2]+offsetInDays,0,0,0,0,0,-1)
+    startOfDayWithOffsetSecs  = time.mktime(startOfDayWithOffsetTuple)
+    return startOfDayWithOffsetSecs
+    
+# This is a GENERATOR not a function
+def walkBackDaysGenerator(startSecs,stopSecs): # see generators docs
+    # boundary Conditions ?
+    # both directions ?
+    currentSecs = startOfDay(startSecs,0)
+    while True:
+        if (currentSecs<=stopSecs): # < or <= ???????
+            return  # termination of generator
+        yield currentSecs
+        currentSecs = startOfDay(currentSecs,-1)
+    
+        
+if __name__ == "__main__":
 
-cursor.close ()
-conn.close ()
+## shall I get rid of Month ?
+##     So Here is the flow
+##       reference table is watt
+##       find max(stamp) from watt --> that will be our startTime (going backwards)
+##                     --bad name use stop for end of watt
+##     for all days, including (current partial day)
+##     for each scope: tensec,minute,hour,day
+##         filltable(..,start,stop) will do appropriate rounding
+##          TO BE CONTINUED..
+    
+
+    #for daySecs in walkBackDaysGenerator(startOfDay(time.time(),0),startOfDay(time.time(),-2)):
+    for daySecs in walkBackDaysGenerator(time.time(),time.time()-(86400*2)):
+        tomorrow = startOfDay(daySecs,1)
+        print "  GMT:[%s GMT] Local:[%s]   lasts %.1f hours" % (
+            time.strftime("%Y-%m-%d %H:%M:%S",time.gmtime(daySecs)),
+            time.strftime("%Y-%m-%d %H:%M:%S %Z day:%j",time.localtime(daySecs)),
+            (tomorrow-daySecs)/3600
+            )
+
+    print "Done"
+    sys.exit(0)
+
+    conn = MySQLdb.connect (host="127.0.0.1",user="aviso",passwd="",db="ted")
+    cursor = conn.cursor ()
+
+    #dropAndCreateTables()
+    fillTables()
+
+    cursor.close ()
+    conn.close ()
 
 
 
