9873f3ff904275d4d4b25312979c584bac197dca
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2008-11-28 02:58:12 +0000

    moved ReadTEDService.py to ted_service and watt table output in GMT

--- trunk/green/scalr-utils/ReadTEDService.py
+++ trunk/green/scalr-utils/ReadTEDService.py
@@ -21,18 +21,43 @@ import urllib
 from xml.dom import minidom  
 import MySQLdb
 
-#def getkWAndVFromTedService():
-def getTimeWattsAndVoltsFromTedService():
+def tableExists(tablename):
+        exists = getScalar("show tables like '%s'" % tablename) is not None
+        return exists
+    
+def checkOrCreateTable(tablename):
+        exists = tableExists(tablename)
+        if exists:
+                logInfo(" Table %s is OK" % (tablename))
+                return
+
+        # I removed table dropping code (safety)
+
+        ddl = """CREATE TABLE %s (
+stamp datetime NOT NULL default '1970-01-01 00:00:00',
+watt int(11) NOT NULL default '0',
+PRIMARY KEY %sByStamp (stamp)
+);
+""" % (tablename,tablename)
+        cursor.execute(ddl)
+        logInfo(" Created %s table" % (tablename))
+
+def getScalar(sql):
+    cursor.execute(sql)
+    row = cursor.fetchone()
+    if row is None: return None
+    return row[0]
+
+def getGMTTimeWattsAndVoltsFromTedService():
 	TED_DASHBOARDDATA_URL = 'http://aria.dl.sologlobe.com:9090/DashboardData'
 	ISO_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 	usock = urllib.urlopen(TED_DASHBOARDDATA_URL)
 	xmldoc = minidom.parse(usock)                              
 	usock.close()                                              
-	# print xmldoc.toxml() 
-	# print ""
-	# print "Extracting KWNow and VrmsNowDsp"
 
-	isodatestr = datetime.datetime.now().strftime(ISO_DATE_FORMAT) 
+	#isodatestr = datetime.datetime.now().strftime(ISO_DATE_FORMAT) 
+	isodatestr = time.strftime(ISO_DATE_FORMAT,time.gmtime(time.time())) 
+
 	kWattStr = xmldoc.getElementsByTagName('KWNow')[0].childNodes[0].nodeValue
 	voltStr = xmldoc.getElementsByTagName('VrmsNowDsp')[0].childNodes[0].nodeValue
 	#print "%s\t%s\t%s" % (isodatestr, kWattStr, voltStr) 
@@ -40,67 +65,64 @@ def getTimeWattsAndVoltsFromTedService():
 	volts  = string.atof(voltStr)
 	return (isodatestr , watts,volts)
 
-usage = 'python %s  ( --duration <secs> | --forever)' % sys.argv[0]
-conn = MySQLdb.connect (host = "127.0.0.1",
-                        user = "aviso",
-                        passwd = "",
-                        db = "ted")
-cursor = conn.cursor ()
-
-if False:
-        tablename='tedlive'
-        ddl = """
-        CREATE TABLE %s (
-        stamp datetime NOT NULL default '1970-01-01 00:00:00',
-        watt int(11) NOT NULL default '0',
-        PRIMARY KEY wattByStamp (stamp)
-        );
-""" % (tablename)
-        #cursor.execute ("DROP TABLE IF EXISTS %s" % tablename)
-        cursor.execute(ddl)
-        print "ddl executed for %s " % (tablename)
-else:
-        pass
-
-# parse command line options
-try:
-	opts, args = getopt.getopt(sys.argv[1:], "", ["duration=", "forever"])
-except getopt.error, msg:
-	logError('error msg: %s' % msg)
-	logError(usage)
-	sys.exit(2)
-
-duration=1
-
-for o, a in opts:
-	if o == "--duration":
-		duration = string.atol(a)
-	elif o == "--forever":
-		duration = -1
-
-print "duration is %d" % duration
-start = time.time()
-while True:
-	datetimenow = datetime.datetime.now()
-	now=time.time()
-	if duration>0 and (now-start)>duration:
-		break
-	(stamp, watts,volts) = getTimeWattsAndVoltsFromTedService()
-	print "%s --- %s\t%d\t%.1f" % (datetimenow,stamp, watts, volts) 
+if __name__ == "__main__":
+        usage = 'python %s  ( --duration <secs> | --forever)' % sys.argv[0]
+        conn = MySQLdb.connect (host = "127.0.0.1",
+                                user = "aviso",
+                                passwd = "",
+                                db = "ted")
+        cursor = conn.cursor ()
         
-        tablename='tedlive'
-        cursor.execute("INSERT IGNORE INTO %s (stamp, watt) VALUES ('%s', '%d')" % (tablename,stamp,watts))
+        # tablenames: watt, ted_service
+        # insert into BOTH tables
+        tablenames=['watt','ted_service']
 
-	now=time.time()
-	if duration>0 and (now-start)>duration:
-		break
-	# sleep to hit the second on the nose:
-	(frac,dummy) = math.modf(now)
-	desiredFractionalOffset = .1
-	delay = 1-frac + desiredFractionalOffset
-	# delay after first iteration should be 1-service time
-	#print "delay: %s  %s" % (delay,now)
-	time.sleep(delay)
+        for tablename in tablenames:
+                checkOrCreateTable(tablename);
+
+
+        # parse command line options
+        try:
+                opts, args = getopt.getopt(sys.argv[1:], "", ["duration=", "forever"])
+        except getopt.error, msg:
+                logError('error msg: %s' % msg)
+                logError(usage)
+                sys.exit(2)
+
+        # default value (forever-> duration=-1
+        duration=1
+
+        for o, a in opts:
+                if o == "--duration":
+                        duration = string.atol(a)
+                elif o == "--forever":
+                        duration = -1
+
+        print "duration is %d" % duration
+        start = time.time()
+
+        while True:
+                datetimenow = datetime.datetime.now()
+                now=time.time()
+                if duration>0 and (now-start)>duration:
+                        break
+                (stamp, watts,volts) = getGMTTimeWattsAndVoltsFromTedService()
+                print "%s --- %s\t%d\t%.1f" % (datetimenow,stamp, watts, volts) 
+
+                # insert into BOTH tables
+                for tablename in tablenames:
+                        sql = "INSERT IGNORE INTO %s (stamp, watt) VALUES ('%s', '%d')" % (
+                                tablename,stamp,watts)
+                        cursor.execute(sql)
+
+                now=time.time()
+                if duration>0 and (now-start)>duration:
+                        break
+                # sleep to hit the second on the nose:
+                (frac,dummy) = math.modf(now)
+                desiredFractionalOffset = .1
+                delay = 1-frac + desiredFractionalOffset
+                time.sleep(delay)
 
 print "Done; lasted %f" % (time.time()-start)
 
