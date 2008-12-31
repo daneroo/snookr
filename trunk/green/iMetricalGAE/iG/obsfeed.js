f8c20d909f6813758c1463f0b4bf4a1261f35a48
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2008-12-31 20:58:33 +0000

    added timer and jquery stuff

---
+++ trunk/green/iMetricalGAE/iG/obsfeed.js
@@ -0,0 +1,151 @@
+/* Obsfeed parser, depends on jQuery
+ * Daniel Lauzon, 2008
+ */
+
+var globalurl = "http://imetrical.appspot.com/feeds?owner=daniel";
+// while true; do curl -o feeds.xml "http://imetrical.appspot.com/feeds?owner=daniel"; sleep 3; done
+// globalurl = "feeds.xml";
+
+function makeJQueryRequest() {
+    $.ajax({
+        type: "GET",
+        url: globalurl,
+        dataType: "xml",
+        success: function(xmlData) {
+
+
+            var feeds=[]; // result array
+
+            var feedList = xmlData.getElementsByTagName("feed");
+            for (var i = 0; i < feedList.length ; i++) {
+                var feed = {
+                    name: feedList.item(i).getAttribute("name"),
+                    stamp: feedList.item(i).getAttribute("stamp").substring(11,19),
+                    value: feedList.item(i).getAttribute("value"),
+                }
+                feeds.push(feed);
+            }
+
+            var html = "<table>";
+            html += "<tr><th>Scope</th><th>Stamp</th><th>W</th><th>kWh/d</th></tr>"
+            for (var i = 0; i < feeds.length; i++) {
+                var f = feeds[i];
+                html += "<tr><td>"+f.name+"</td><td>"+f.stamp+"</td><td>"+f.value+"</td><td>"+(f.value*24.0/1000)+"</td></tr>"
+            }
+            html += "</table>";
+            $("div.obs").hide().html(html).fadeIn("fast");
+        }
+    });
+
+
+}
+
+
+
+function makeNativeDOMRequest() {
+    var url = "http://imetrical.appspot.com/feeds?owner=daniel";
+    //var url = "http://dl.sologlobe.com:9999/iMetrical/feeds.php";
+    //var url = "http://192.168.5.2/iMetrical/feeds.php";
+    xmlhttp=new XMLHttpRequest();
+
+    if (xmlhttp!=null)  {
+        xmlhttp.onreadystatechange=onResponseNative;
+        xmlhttp.open("GET",url,true);
+        xmlhttp.send(null);
+    } else  {
+        alert("Your browser does not support XMLHTTP.");
+    }
+};
+
+function onResponseNative() {
+    if(xmlhttp.readyState!=4) return;
+    if(xmlhttp.status!=200) {
+        alert("Problem retrieving XML data");
+        return;
+    }
+    var domdata = xmlhttp.responseXML.documentElement;
+    responseCommon(domdata);
+    var refreshRate = 5;
+    setTimeout("makeNativeDOMRequest()", refreshRate*1000)
+};
+
+function makeDOMRequest() {
+    var params = {};
+    params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.DOM;
+    var url = "http://imetrical.appspot.com/feeds?owner=daniel";
+    gadgets.io.makeRequest(url, response, params);
+};
+
+function responseIG(obj) {
+    // obj.data contains a Document DOM element corresponding to the
+    // page that was requested
+    var domdata = obj.data;
+    responseCommon(domdata)
+};
+
+function responseCommon(domdata) {
+    var html = "<table>";
+    html += "<tr><th>Scope</th><th>Stamp</th><th>W</th><th>kWh/d</th></tr>"
+    var feedList = domdata.getElementsByTagName("feed");
+    for (var i = 0; i < feedList.length ; i++) {
+        // For each <food> node, get child nodes.
+        var name = feedList.item(i).getAttribute("name");
+        var stamp = feedList.item(i).getAttribute("stamp").substring(11,19);
+        var value = feedList.item(i).getAttribute("value");
+
+        html += "<tr><td>"+name+"</td><td>"+stamp+"</td><td>"+value+"</td><td>"+(value*24.0/1000)+"</td></tr>"
+    }
+    html += "</table>";
+
+    var latestStampStr = feedList.item(0).getAttribute("stamp")
+    var dstamp = new Date();
+    //dstamp.setISO8601("2005-03-26T19:51:34Z");
+    //dstamp.setISO8601("2008-12-30T04:23:13Z");
+    dstamp.setISO8601(latestStampStr);
+
+    var refreshRate = 5;
+    html += "<pre>Refresh Rate: "+refreshRate+" latency: "+(new Date().getTime()-dstamp.getTime())/1000+"<pre>"
+
+    document.getElementById('content_div').innerHTML = html;
+    document.getElementById('status_div').innerHTML = dstamp+"<br>"+new Date();
+
+};
+
+// Date Handling
+Date.prototype.setISO8601 = function (string) {
+    var regexp = "([0-9]{4})(-([0-9]{2})(-([0-9]{2})" +
+    "(T([0-9]{2}):([0-9]{2})(:([0-9]{2})(\.([0-9]+))?)?" +
+    "(Z|(([-+])([0-9]{2}):([0-9]{2})))?)?)?)?";
+    var d = string.match(new RegExp(regexp));
+
+    var offset = 0;
+    var date = new Date(d[1], 0, 1);
+
+    if (d[3]) {
+        date.setMonth(d[3] - 1);
+    }
+    if (d[5]) {
+        date.setDate(d[5]);
+    }
+    if (d[7]) {
+        date.setHours(d[7]);
+    }
+    if (d[8]) {
+        date.setMinutes(d[8]);
+    }
+    if (d[10]) {
+        date.setSeconds(d[10]);
+    }
+    if (d[12]) {
+        date.setMilliseconds(Number("0." + d[12]) * 1000);
+    }
+    if (d[14]) {
+        offset = (Number(d[16]) * 60) + Number(d[17]);
+        offset *= ((d[15] == '-') ? 1 : -1);
+    }
+
+    offset -= date.getTimezoneOffset();
+    time = (Number(date) + (offset * 60 * 1000));
+    this.setTime(Number(time));
+};
+
