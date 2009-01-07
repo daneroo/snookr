8a2f8a8dc199619cf52179b7e7c1fb62765fa86f
Daniel Lauzon <daneroo@users.noreply.github.com>
2009-01-07 05:56:12 +0000

    possible to put google-chart on fbml profile

--- green/scalr-utils/facebook/updateRefhandle.php
+++ green/scalr-utils/facebook/updateRefhandle.php
@@ -3,7 +3,10 @@ require_once 'facebook.php';
 
 $now = time();
 $stamp = date('Y-m-d',$now).'T'.date('H:i:s',$now).'Z';
-$fbml = "Last Updated at ".$stamp;
+#$graphurl = "http://chart.apis.google.com/chart?cht=lc&chs=180x120&chd=e:vCvCutw90y0e0Jzgxmx6kzijh6h6h6h6hmhmhRhmhmhmhmh6h6hmhmhmh6h6h6h6h6h6hmhmhmhRhRhRhRhRg9h6h6ijijijijiji4fXeFdwdwYpXXVcVcVc&chtt=Hour&chts=7f93bc,16&chco=3b5998&chxt=y&chxl=0:%7ckW%7c0.5%7c1.0%7c1.5%7c2.0";
+#$fbml= "<img src='$graphurl' >";
+$fbml='';
+$fbml .= " Last Updated at ".$stamp;
 echo "Update Facebook iMetrical refHandle:myhandle to:\n";
 echo "  ".$fbml."\n";
 
