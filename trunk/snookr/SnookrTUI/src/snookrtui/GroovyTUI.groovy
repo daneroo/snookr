b5d1484c75a4c4c266119efcfb2f44c7c60ea06d
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2009-03-23 16:47:24 +0000

    New Text UI

---
+++ trunk/snookr/SnookrTUI/src/snookrtui/GroovyTUI.groovy
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package snookrtui
+
+/**
+ *
+ * @author daniel
+ */
+class GroovyTUI {
+	public sayHello(){
+        println("Groovy says Hello")
+    }
+}
+
