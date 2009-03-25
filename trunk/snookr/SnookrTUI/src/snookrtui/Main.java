b2b5e6c78c2507513a0a898e1e13538921d36f6b
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2009-03-25 20:47:16 +0000

    VT100 console...

--- trunk/snookr/SnookrTUI/src/snookrtui/Main.java
+++ trunk/snookr/SnookrTUI/src/snookrtui/Main.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package snookrtui;
 
 import java.io.File;
@@ -17,11 +16,19 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            try {
+                Thread.sleep(100);
+            } catch (Exception e) {
+            }// wait a little
+            progress("Message: " + String.format("%5d", i));
+        }
+        System.exit(0);
         //Report:
         new ImageClassification().run();
         System.exit(0);
-        
-        if (args.length<1) {
+
+        if (args.length < 1) {
             System.err.println("Please Specify baseDir, as in:");
             System.err.println("  java xx.jar /Volumes/DarwinScratch/photo");
             System.err.println("  java xx.jar /home/daniel/media");
@@ -40,5 +47,15 @@ public class Main {
         //Report:
         new ImageClassification().run();
     }
+    static final byte besc[] = {27};
+    static final String esc = new String(besc);
+    static final String clearline = esc + "[K";
 
+    public static void progress(String msg) {
+        msg = " " + msg; // room for cursor.
+        int size = msg.length();
+        String rewind = esc + "[" + size + "D";
+        System.err.print(clearline + msg + rewind);
+        System.err.flush();
+    }
 }
