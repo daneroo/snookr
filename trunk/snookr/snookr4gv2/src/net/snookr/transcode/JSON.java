e0fc93a99b5109835448d73c965391d1c897070f
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2009-04-11 18:28:52 +0000

    Added First test for JSON, and generated equals+hashcode for FSImage

--- trunk/snookr/snookr4gv2/src/net/snookr/transcode/JSON.java
+++ trunk/snookr/snookr4gv2/src/net/snookr/transcode/JSON.java
@@ -26,17 +26,24 @@ import net.snookr.model.FSImage;
  */
 public class JSON {
 
-    Gson gson;
-
+    private Gson gson;
     public JSON() {
-        //gson = new Gson();
-        gson = new GsonBuilder().setPrettyPrinting().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
-        //gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+        this(false);
+    }
+
+    public JSON(boolean pretty) { // default false: compact
+        GsonBuilder gsb = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss");
+        if (pretty) {
+            gsb.setPrettyPrinting();
+        }
+        gson = gsb.create();
+    }
+
+    public Gson getGson() {
+        return gson;
     }
 
     public String encode(List l) {
-        //String json = gson.toJson(list,listTpe);// not sure what more this does
-        //return gson.toJson(l);
         StringBuffer sb = new StringBuffer();
         encode(l, sb);
         return sb.toString();
@@ -58,66 +65,4 @@ public class JSON {
         return l;
     }
 
-    /* Testing code */
-    public void test() {
-        System.out.println("Hello transcode.JSON");
-        testPrimitives();
-        testObject();
-        testFSImageList();
-    }
-
-    private void show(String s) {
-        System.out.println(s);
-    }
-
-    private FSImage randFSImage() {
-        int r = 1000000 + new Random().nextInt(1000000);
-        FSImage fsima = new FSImage();
-        fsima.fileName = "/pathToFile/image" + r + ".jpg";
-        fsima.size = new Long(r);
-        fsima.md5 = "0e7eded2e5283be3f6b716c71e7e1c1c";
-        fsima.lastModified = new Date(new Date().getTime() + r);
-        fsima.taken = fsima.lastModified;
-        return fsima;
-    }
-
-    private void testObject() {
-        show("Test FSImage Object");
-        FSImage fsima = randFSImage();
-        String json = gson.toJson(fsima);
-        show("  " + fsima + " \n    -->\n  " + json);
-
-        FSImage fsima2 = gson.fromJson(json, FSImage.class);
-        show("decoded --> " + fsima2);
-    }
-
-    private void testFSImageList() {
-        show("Test FSImage List");
-        List list = new ArrayList();
-        list.add(randFSImage());
-        list.add(randFSImage());
-        list.add(randFSImage());
-
-        String json = encode(list);
-        show("  list[3]{fsima^3} \n    -->\n  " + json);
-
-        List list2 = decodeFSImageList(json);
-        show("decoded --> ");
-        for (Object o : list2) {
-            show("   - " + o.getClass() + " : " + o);
-        }
-    }
-
-    private void testPrimitives() {
-        show("Test Primitives");
-        show("1 --> " + gson.toJson(1));
-        show("abcd --> " + gson.toJson("abcd"));
-        show("Long(10) --> " + gson.toJson(new Long(10)));
-        int[] values = {1};
-        show("int[] {1} --> " + gson.toJson(values));
-    }
-
-    public static void main(String[] args) {
-        new JSON().test();
-    }
 }
