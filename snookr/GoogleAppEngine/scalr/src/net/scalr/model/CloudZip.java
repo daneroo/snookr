6d5c062e9830c06c30e107c2b4dfe9bcb24cb89f
Daniel Lauzon <daneroo@users.noreply.github.com>
2009-05-17 20:05:14 +0000

    CloudZip moving to updates to DAO

--- snookr/GoogleAppEngine/scalr/src/net/scalr/model/CloudZip.java
+++ snookr/GoogleAppEngine/scalr/src/net/scalr/model/CloudZip.java
@@ -9,23 +9,35 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import javax.jdo.annotations.Extension;
+import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
-import net.scalr.MD5;
 
 /**
  *
  * @author daniel
  */
 @PersistenceCapable(identityType = IdentityType.APPLICATION)
+// Unique might include owner, later
+//@Unique(name="ZIP_NAME_IDX", members={"name"})
 public class CloudZip {
 
     @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    @Extension(vendorName="datanucleus", key="gae.encoded-pk", value="true")
+    private String encodedKey;
+
     @Persistent
+    @Extension(vendorName="datanucleus", key="gae.pk-name", value="true")
     private String name;
 
+
+    public String getKeyDescription(){
+        return String.format("Key as String: %s | name: %s",encodedKey,name);
+    }
     public String getName() {
         return name;
     }
@@ -49,31 +61,17 @@ public class CloudZip {
     @Persistent
     private List<CloudZipEntry> entries;
 
-    public List<CloudZipEntry> getEntriesAsList() {
+    public List<CloudZipEntry> getEntries() {
         return entries;
     }
-    public Map<String, byte[]> getEntries() {
-        Map<String, byte[]> map = new LinkedHashMap<String, byte[]>(entries.size());
-        for (CloudZipEntry ce : entries) {
-            byte[] content = ce.getContent();
-            map.put(ce.getName(), content);
-            //String md5sum = MD5.digest(content);
-            //System.out.println("Get: " + ce.getName() + " length: " + content.length + " md5: " + md5sum);
-        }
-        return map;
+    public void setEntries(List<CloudZipEntry> entries) {
+        this.entries = entries;
     }
 
-    public void setEntries(Map<String, byte[]> entries) {
-        List<CloudZipEntry> list = new ArrayList<CloudZipEntry>(entries.size());
-        for (Map.Entry<String, byte[]> e : entries.entrySet()) {
-            list.add(new CloudZipEntry(e.getKey(), e.getValue()));
-            //String md5sum = MD5.digest(e.getValue());
-            //System.out.println("Set: " + e.getKey() + " length: " + e.getValue().length + " md5: " + md5sum);
-        }
-        this.entries = list;
+    public CloudZip(String name) {
+        this(name,new ArrayList<CloudZipEntry>());
     }
-
-    public CloudZip(String name, Map<String, byte[]> entries) {
+    public CloudZip(String name, List<CloudZipEntry> entries) {
         this.name = name;
         setEntries(entries);
     }
