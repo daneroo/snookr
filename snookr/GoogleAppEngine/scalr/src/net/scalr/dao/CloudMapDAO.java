85c142ef87392d035aeaf454da28fa64196cfce3
Daniel Lauzon <daneroo@users.noreply.github.com>
2009-05-21 00:49:57 +0000

    moving to CloudMap

--- snookr/GoogleAppEngine/scalr/src/net/scalr/dao/CloudMapDAO.java
+++ snookr/GoogleAppEngine/scalr/src/net/scalr/dao/CloudMapDAO.java
@@ -4,9 +4,18 @@
  */
 package net.scalr.dao;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.logging.Logger;
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
+import javax.jdo.Transaction;
+import net.scalr.JSON;
 import net.scalr.model.CloudMap;
 
 /**
@@ -15,34 +24,81 @@ import net.scalr.model.CloudMap;
  */
 public class CloudMapDAO {
 
-    public CloudMap get(String name) {
-        PersistenceManager pm = PMF.get().getPersistenceManager();
-        try {
-            CloudMap clm = pm.getObjectById(CloudMap.class, name);
-            //System.out.println(" DAO Fetched: "+clm.getName()+" --> "+clm.getContent());
-            return clm;
-        } finally {
-            pm.close();
+    private static final Logger log =
+            Logger.getLogger(CloudMapDAO.class.getName());
+
+    private byte[] ramp(int sz) {
+        byte[] content = new byte[sz];
+        for (int i = 0; i < content.length; i++) {
+            content[i] = (byte) (i % 256);
         }
+        return content;
+    }
+
+    private byte[] rand(int sz) {
+        byte[] content = new byte[sz];
+        rnd.nextBytes(content);
+        return content;
+    }
+    Random rnd = new Random(123456);
+
+//        int constSz = 512 * 1024;
+    public String testBigRandRW() {
+        return testRandRW("group-rw-big",512*1024, 30, 10);
+    }
+    public String testSmallRandRW() {
+        return testRandRW("group-rw-small",1024, 300, 100);
+    }
+    public String testRandRW(String group,int writeSize,int totalEntries,int entriesToWrite) {
+        // random read-write
+        for (int i = 0; i < entriesToWrite; i++) {
+            int index = new Random().nextInt(totalEntries);
+            CloudMap clm = new CloudMap(group, "const:sz:" + String.format("%04d", index), ramp(writeSize));
+            createOrUpdate(clm);
+        }
+        return makeManifest(getAll());
+    }
+
+    public String testCRD() {
+        System.out.println("Before Create:" + makeManifest(getAll()));
+        // testing duplicate group/name
+        for (int i = 0; i < 3; i++) {
+            CloudMap clm = new CloudMap("group-dup", "name1", ("content-" + i).getBytes());
+            createOrUpdate(clm);
+        }
+
+        // testing size
+        int maxRandSz = 1024 * 1024 * 1;
+        for (int sz = 1024; sz < maxRandSz; sz *= 2) {
+            CloudMap clm = new CloudMap("group-size", "rand:sz:" + String.format("%07d", sz), rand(sz));
+            createOrUpdate(clm);
+        }
+        int maxRampSz = 1024 * 1024 * 10;
+        for (int sz = 1024; sz < maxRampSz; sz *= 2) {
+            CloudMap clm = new CloudMap("group-size", "ramp:sz:" + String.format("%07d", sz), ramp(sz));
+            createOrUpdate(clm);
+        }
+
+        System.out.println("After Create-Before Delete:" + makeManifest(getAll()));
+        return makeManifest(getAll());
+    }
+
+    public CloudMap get(String name) {
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     public void delete(String name) {
-        PersistenceManager pm = PMF.get().getPersistenceManager();
-        try {
-            CloudMap clm = pm.getObjectById(CloudMap.class, name);
-            if (clm != null) {
-                pm.deletePersistent(clm);
-            }
-        } finally {
-            pm.close();
-        }
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     public void createOrUpdate(CloudMap clm) {
-        //CloudMap persisted = get(clm.getName());
-        // this is the default behaviour : overwite!
         PersistenceManager pm = PMF.get().getPersistenceManager();
         try {
+            // first delete duplicates - there should only ever be one !
+            List<CloudMap> toDelete = internalGetEntry(pm, clm.getGroup(), clm.getName());
+            internalDelete(pm, toDelete);
+
+            // Now make the new one persistent
             pm.makePersistent(clm);
         } finally {
             pm.close();
@@ -52,21 +108,131 @@ public class CloudMapDAO {
     public List<CloudMap> getAll() {
         PersistenceManager pm = PMF.get().getPersistenceManager();
         try {
-            Query query = pm.newQuery(CloudMap.class);
-            //Query query = pm.newQuery("select from CloudMap");
-            List<CloudMap> results = (List<CloudMap>) query.execute();
-            return results;
+            List<CloudMap> all = internalGetAll(pm);
+            // Sort this thing
+            return sortByGroupAndName(all);
         } finally {
             pm.close();
         }
-
     }
 
-    public void print(List<CloudMap> list) {
-        System.out.println("List has " + list.size() + " CloudMap entries");
-        for (CloudMap clm : list) {
-            System.out.println(" " + clm.getName() + " --> " + new String(clm.getContent()));
+    public void deleteAll() {
+        PersistenceManager pm = PMF.get().getPersistenceManager();
+        try {
+            List<CloudMap> all = internalGetAll(pm);
+            internalDelete(pm, all);
+        } finally {
+            pm.close();
+        }
+    }
+
+    private String makeManifest(List<CloudMap> entries) {
+        List<Map<String, String>> manifestList = new ArrayList<Map<String, String>>();
+        //String now = new Date().toString();
+        //log.warning("Making Manifest @ " + now);
+        if (entries != null) {
+            boolean myOwnPrettyJson = true;
+            if (myOwnPrettyJson) {
+                if (entries.size() == 0) {
+                    return "[]";
+                }
+                StringBuilder sb = new StringBuilder("[\n");
+                for (CloudMap e : entries) {
+                    sb.append(String.format("g:%s n:%s l:%7d md5:%s\n", e.getGroup(), e.getName(), e.getLength(), e.getMd5()));
+                }
+                // remove the last newline
+                sb.append("] sz="+entries.size());
+                return sb.toString();
+            }
+            for (CloudMap e : entries) {
+                if (e == null || e.getName() == null) {
+                    log.warning("Manifest has null entry: " + e.getKeyDescription());
+                } else {
+                    log.info("Manifest has good entry: " + e.getKeyDescription());
+                    Map<String, String> manifestEntry = new LinkedHashMap<String, String>();
+                    manifestEntry.put("group", e.getGroup());
+                    manifestEntry.put("name", e.getName());
+                    manifestEntry.put("length", "" + e.getLength());
+                    manifestEntry.put("md5", e.getMd5());
+                    manifestList.add(manifestEntry);
+                }
+            }
+            return new JSON().encode(manifestList);
+        } else { // null entries
+            return "[]";
+        }
+    }
+
+    // this is only meant for persistent entries!
+    private void internalDelete(PersistenceManager pm, List<CloudMap> entries) {
+        for (CloudMap entry : entries) {
+            log.info("Deleting CloudMap Entry:" + entry.getKeyDescription());
+        //pm.deletePersistent(entry);
+        }
+        pm.deletePersistentAll(entries);
+    }
+
+    private List<CloudMap> internalGetEntry(PersistenceManager pm, String group, String name) {
+        Query query = pm.newQuery(CloudMap.class,
+                "group == groupParam && name == nameParam");
+        query.declareParameters("java.lang.String groupParam,java.lang.String nameParam");
+        List<CloudMap> entries = (List<CloudMap>) query.execute(group,name);
+        // hit this thing....
+        entries.size();
+        return entries;
+    }
+    // unused replaced by different Query
+    private List<CloudMap> internalGetEntryWithFilter(PersistenceManager pm, String group, String name) {
+        List<CloudMap> entries = internalGetGroupUnsorted(pm, group);
+        List<CloudMap> matched = new ArrayList<CloudMap>(1);
+        for (CloudMap entry : entries) {
+            if (name.equals(entry.getName())) {
+                matched.add(entry);
+            }
+        }
+        return matched;
+    }
+
+    /* This one is sortByGroupAndName
+     */
+    private List<CloudMap> internalGetGroup(PersistenceManager pm, String group) {
+        return sortByGroupAndName(internalGetGroupUnsorted(pm, group));
+    }
+
+    /* Sort a retuned copy by group and name
+     */
+    public List<CloudMap> sortByGroupAndName(List<CloudMap> list) {
+        class GroupAndNameComparator implements Comparator<CloudMap> {
+
+            public int compare(CloudMap o1, CloudMap o2) {
+                int cmp = o1.getGroup().compareTo(o2.getGroup());
+                if (cmp == 0) {
+                    cmp = o1.getName().compareTo(o2.getName());
+                }
+                return cmp;
+            }
         }
 
+        List<CloudMap> sortedListCopy = new ArrayList<CloudMap>(list);
+        Collections.sort(sortedListCopy, new GroupAndNameComparator());
+        return sortedListCopy;
+    }
+
+    private List<CloudMap> internalGetGroupUnsorted(PersistenceManager pm, String group) {
+        Query query = pm.newQuery(CloudMap.class,
+                "group == groupParam");
+        query.declareParameters("java.lang.String groupParam");
+        List<CloudMap> entries = (List<CloudMap>) query.execute(group);
+        // hit this thing....
+        entries.size();
+        return entries;
+    }
+
+    private List<CloudMap> internalGetAll(PersistenceManager pm) {
+        Query query = pm.newQuery(CloudMap.class);
+        List<CloudMap> entries = (List<CloudMap>) query.execute();
+        // hit this thing....
+        entries.size();
+        return entries;
     }
 }
--- snookr/GoogleAppEngine/scalr/src/net/scalr/model/CloudMap.java
+++ snookr/GoogleAppEngine/scalr/src/net/scalr/model/CloudMap.java
@@ -5,10 +5,15 @@
 package net.scalr.model;
 
 import com.google.appengine.api.datastore.Blob;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.PrimaryKey;
 import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
+import net.scalr.GZIP;
+import net.scalr.MD5;
 
 /**
  *
@@ -18,27 +23,68 @@ import javax.jdo.annotations.Persistent;
 public class CloudMap {
 
     @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+    @Persistent
+    private String group;
     @Persistent
     private String name;
+    @Persistent
+    private int length;
+    @Persistent
+    private String md5;
+    //@Persistent(defaultFetchGroup="true")
+    @Persistent()
+    private Blob content;
+
+    public String getKeyDescription() {
+        return String.format("Key: %s | group: %s name: %s", KeyFactory.keyToString(key), group, name);
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public String getGroup() {
+        return group;
+    }
 
     public String getName() {
         return name;
     }
 
-    @Persistent(defaultFetchGroup="true")
-    private Blob content;
+    public int getLength() {
+        return length;
+    }
+
+    public String getMd5() {
+        return md5;
+    }
 
     public byte[] getContent() {
-        if (content==null) return null;
-        return content.getBytes();
+        if (content == null) {
+            return null;
+        }
+        //return content.getBytes();
+        byte[] uncompressed = new GZIP().gunzip(content.getBytes());
+        //System.out.println("  Uncompressed "+content.getBytes().length+"->"+uncompressed.length);
+        return uncompressed;
     }
 
     public void setContent(byte[] content) {
-        this.content = new Blob(content);
+        this.length = content.length;
+        this.md5 = MD5.digest(content);
+        byte[] compressed = new GZIP().gzip(content);
+        //System.out.println("  Compressed "+content.length+"->"+compressed.length);
+        this.content = new Blob(compressed);
     }
 
-    public CloudMap(String name,byte[] content){
-        this.name=name;
-        this.content=new Blob(content);
+    public CloudMap(String group, String name, byte[] content) {
+        this.group = group;
+        this.name = name;
+        setContent(content);
+    }
+
+    private CloudMap() {
     }
 }
