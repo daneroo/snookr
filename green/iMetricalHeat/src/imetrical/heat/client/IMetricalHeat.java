75344bddc2c5315a6b5359f73de8a040e31f7df6
Daniel Lauzon <daneroo@users.noreply.github.com>
2009-06-01 18:13:47 +0000

    First checkin

---
+++ green/iMetricalHeat/src/imetrical/heat/client/IMetricalHeat.java
@@ -0,0 +1,221 @@
+package imetrical.heat.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DecoratorPanel;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.DisclosurePanel;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RadioButton;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.FlexTable.FlexCellFormatter;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class IMetricalHeat implements EntryPoint {
+	/**
+	 * The message displayed to the user when the server cannot be reached or
+	 * returns an error.
+	 */
+	private static final String SERVER_ERROR = "An error occurred while "
+			+ "attempting to contact the server. Please check your network "
+			+ "connection and try again.";
+
+	/**
+	 * Create a remote service proxy to talk to the server-side Greeting service.
+	 */
+	private final GreetingServiceAsync greetingService = GWT
+			.create(GreetingService.class);
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		final Button sendButton = new Button("Send");
+		final TextBox nameField = new TextBox();
+		final VerticalPanel panel = new VerticalPanel();
+	    panel.addStyleName("demo-panel-padded");
+	    panel.setSize("200px", "120px");
+	    panel.add(getListBox(false));
+	    panel.add(getListBox(true));
+	    panel.add(createAdvancedForm());
+
+	    nameField.setText("GWT User");
+
+		// We can add style names to widgets
+		sendButton.addStyleName("sendButton");
+
+		// Add the nameField and sendButton to the RootPanel
+		// Use RootPanel.get() to get the entire body element
+		RootPanel.get("nameFieldContainer").add(nameField);
+		RootPanel.get("sendButtonContainer").add(sendButton);
+		RootPanel.get("editorPanelContainer").add(panel);
+
+		// Focus the cursor on the name field when the app loads
+		nameField.setFocus(true);
+		nameField.selectAll();
+
+		// Create the popup dialog box
+		final DialogBox dialogBox = new DialogBox();
+		dialogBox.setText("Remote Procedure Call");
+		dialogBox.setAnimationEnabled(true);
+		final Button closeButton = new Button("Close");
+		// We can set the id of a widget by accessing its Element
+		closeButton.getElement().setId("closeButton");
+		final Label textToServerLabel = new Label();
+		final HTML serverResponseLabel = new HTML();
+		VerticalPanel dialogVPanel = new VerticalPanel();
+		dialogVPanel.addStyleName("dialogVPanel");
+		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
+		dialogVPanel.add(textToServerLabel);
+		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
+		dialogVPanel.add(serverResponseLabel);
+		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
+		dialogVPanel.add(closeButton);
+		dialogBox.setWidget(dialogVPanel);
+
+		// Add a handler to close the DialogBox
+		closeButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				dialogBox.hide();
+				sendButton.setEnabled(true);
+				sendButton.setFocus(true);
+			}
+		});
+
+		// Create a handler for the sendButton and nameField
+		class MyHandler implements ClickHandler, KeyUpHandler {
+			/**
+			 * Fired when the user clicks on the sendButton.
+			 */
+			public void onClick(ClickEvent event) {
+				sendNameToServer();
+			}
+
+			/**
+			 * Fired when the user types in the nameField.
+			 */
+			public void onKeyUp(KeyUpEvent event) {
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					sendNameToServer();
+				}
+			}
+
+			/**
+			 * Send the name from the nameField to the server and wait for a response.
+			 */
+			private void sendNameToServer() {
+				sendButton.setEnabled(false);
+				String textToServer = nameField.getText();
+				textToServerLabel.setText(textToServer);
+				serverResponseLabel.setText("");
+				greetingService.greetServer(textToServer,
+						new AsyncCallback<String>() {
+							public void onFailure(Throwable caught) {
+								// Show the RPC error message to the user
+								dialogBox
+										.setText("Remote Procedure Call - Failure");
+								serverResponseLabel
+										.addStyleName("serverResponseLabelError");
+								serverResponseLabel.setHTML(SERVER_ERROR);
+								dialogBox.center();
+								closeButton.setFocus(true);
+							}
+
+							public void onSuccess(String result) {
+								dialogBox.setText("Remote Procedure Call");
+								serverResponseLabel
+										.removeStyleName("serverResponseLabelError");
+								serverResponseLabel.setHTML(result);
+								dialogBox.center();
+								closeButton.setFocus(true);
+							}
+						});
+			}
+		}
+
+		// Add a handler to send the name to the server
+		MyHandler handler = new MyHandler();
+		sendButton.addClickHandler(handler);
+		nameField.addKeyUpHandler(handler);
+	}
+	
+	ListBox getListBox(boolean dropdown) {
+	    ListBox widget = new ListBox();
+	    widget.addStyleName("demo-ListBox");
+	    widget.addItem("One");
+	    widget.addItem("Two");
+	    widget.addItem("Three");
+	    widget.addItem("Four");
+	    widget.addItem("Five");
+	    if(!dropdown)widget.setVisibleItemCount(3);
+	    return widget;
+	}
+
+	/**
+	   * Create a form that contains undisclosed advanced options.
+	   */
+	  private Widget createAdvancedForm() {
+	    // Create a table to layout the form options
+	    FlexTable layout = new FlexTable();
+	    layout.setCellSpacing(6);
+	    layout.setWidth("300px");
+	    FlexCellFormatter cellFormatter = layout.getFlexCellFormatter();
+
+	    // Add a title to the form
+	    layout.setHTML(0, 0, "Title of the Form");
+	    cellFormatter.setColSpan(0, 0, 2);
+	    cellFormatter.setHorizontalAlignment(0, 0,
+	        HasHorizontalAlignment.ALIGN_CENTER);
+
+	    // Add some standard form options
+	    layout.setHTML(1, 0, "FormName");
+	    layout.setWidget(1, 1, new TextBox());
+	    layout.setHTML(2, 0, "Form Description");
+	    layout.setWidget(2, 1, new TextBox());
+
+	    // Create some advanced options
+	    HorizontalPanel genderPanel = new HorizontalPanel();
+	    String[] genderOptions = new String[]{"male","female"};
+	    for (int i = 0; i < genderOptions.length; i++) {
+	      genderPanel.add(new RadioButton("gender", genderOptions[i]));
+	    }
+	    Grid advancedOptions = new Grid(2, 2);
+	    advancedOptions.setCellSpacing(6);
+	    advancedOptions.setHTML(0, 0, "Location");
+	    advancedOptions.setWidget(0, 1, new TextBox());
+	    advancedOptions.setHTML(1, 0, "Gender");
+	    advancedOptions.setWidget(1, 1, genderPanel);
+
+	    // Add advanced options to form in a disclosure panel
+	    DisclosurePanel advancedDisclosure = new DisclosurePanel(
+	        "Advanced Criteria");
+	    advancedDisclosure.setAnimationEnabled(true);
+	    advancedDisclosure.ensureDebugId("cwDisclosurePanel");
+	    advancedDisclosure.setContent(advancedOptions);
+	    layout.setWidget(3, 0, advancedDisclosure);
+	    cellFormatter.setColSpan(3, 0, 2);
+
+	    // Wrap the contents in a DecoratorPanel
+	    DecoratorPanel decPanel = new DecoratorPanel();
+	    decPanel.setWidget(layout);
+	    return decPanel;
+	  }
+}
