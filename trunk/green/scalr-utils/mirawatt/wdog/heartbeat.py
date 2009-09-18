ab155ff2ef39d7895c8b440e03172d3c8d58a4f4
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2009-09-18 20:46:50 +0000

    heartbeat test

---
+++ trunk/green/scalr-utils/mirawatt/wdog/heartbeat.py
@@ -0,0 +1,32 @@
+
+import sys
+import time
+import random
+
+# DON't use time.strftime with %z with gmtime!
+# How about datetime instead!
+ISO_DATE_FORMAT = '%Y-%m-%dT%H:%M:%S%z'
+
+def log(msg):
+    sys.stdout.write("%s\n"%msg)
+    sys.stdout.flush()
+
+log("Heartbeat is starting")
+while (True):
+    gmtStamp = time.strftime(ISO_DATE_FORMAT,time.gmtime())
+    gmtStamp = time.strftime(ISO_DATE_FORMAT,time.localtime())
+    log("%s heartbeat" % gmtStamp)
+
+    # randomly die
+    if (random.random()<.001):
+        log("%s heartbeat is randomly dying" % gmtStamp)
+        sys.exit(0)
+
+    # randomly wait more than 30 Secs
+    if (random.random()<.001):
+        log("%s heartbeat is randomly hanging > 60 secs" % gmtStamp)
+        time.sleep(60)
+
+    time.sleep(5)
+
+log("Heartbeat is exiting")
