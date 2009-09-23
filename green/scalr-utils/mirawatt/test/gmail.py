f142ea3b2ae9ff98aea62117fae15ebd79562741
Daniel Lauzon <daneroo@users.noreply.github.com>
2009-09-23 04:57:58 +0000

    Trying subversion keyword substitution

--- green/scalr-utils/mirawatt/test/gmail.py
+++ green/scalr-utils/mirawatt/test/gmail.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python
 
+import sys
 import smtplib
 
+version='$Id$'
 smtpHost='smtp.gmail.com'
 smtpPort=587 #port 465 or 587
 smtpUsername='watchdog@mirawatt.com'
 smtpPassword='' # try md5 or something
 smtpFrom = smtpUsername
 smtpTo = 'alerts@mirawatt.com'
+
+if (not smtpPassword):
+    print "SMTP password required: set and rerun"
+    sys.exit()
 
 msg = ("From: %s\r\nTo: %s\r\nSubject:Gateway Test\r\n\r\nThis is a test of the gmail. SMTP Gateway") % (smtpFrom,smtpTo)
 
