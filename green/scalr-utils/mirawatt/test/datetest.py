e8833c072b856f7a8b91986260671a29fee1ba80
Daniel Lauzon <daneroo@users.noreply.github.com>
2009-09-23 19:41:00 +0000

    datetime tests

---
+++ green/scalr-utils/mirawatt/test/datetest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# validate basic datetime manipulations
+# especialy related to timezones
+#
+#  We will first parse an ISO Format
+
+
+import sys
+
+if __name__ == "__main__":
+    print "Starting datetime tests"
