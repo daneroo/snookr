70bfdaa6c7b6fe7753433f817015b509fa7c9ff1
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2009-10-09 05:52:25 +0000

    make iso8601 into a class

--- trunk/green/scalr-utils/mirawatt/iso8601.py
+++ trunk/green/scalr-utils/mirawatt/iso8601.py
@@ -37,6 +37,63 @@ TESTDATEFRAC='2009-10-08T01:04:53.456Z'
 ISO_DATE_FORMAT_Z =   '%Y-%m-%dT%H:%M:%SZ'
 ISO_DATE_FORMAT_NOZ = '%Y-%m-%dT%H:%M:%S'
 
+class iso8601:
+    """
+    A class for manipulating iso 8601 datetimes
+    utc: naive datetime object
+    """
+    def __init__(self,stampStr):    
+        self.utc = self.parse(stampStr)
+        self.lcl = self.local()
+
+    def __str__(self):
+        return "%s : %f : %s" % (self.utc,self.unix_stamp,self.lcl)
+    
+    # use raw strings r"..": which do not escape \'s
+    pattern = re.compile(r"(?P<year>[0-9]{4})-"
+                         r"(?P<month>[0-9]{2})-"
+                         r"(?P<day>[0-9]{2})"
+                         r"T?"
+                         r"(?P<hour>[0-9]{2}):"
+                         r"(?P<minute>[0-9]{2}):"
+                         r"(?P<second>[0-9]{2}([,.][0-9]+)?)"
+                         r"Z"
+                         )
+    def parse(self,stampStr):
+        '''
+        Parse an iso 8601 string and return a naive datetime object
+        representing the UTC tuple for the passed datetime representation
+        TODO: parse seperators,timezone and manipulate datetime tuple
+        '''
+        match = pattern.match(stampStr)
+        if (match):
+            g = match.groupdict()
+            # seconds rounded to int even thoug RE allows fractional seconds
+            tuple_date = datetime.datetime(int(g['year']), int(g['month']), int(g['day']), int(g['hour']), int(g['minute']), int(float(g['second'])))
+            # TODO add offset, use timedelta...
+            return tuple_date
+        else:
+            return None
+
+    def local(self):
+        self.unix_stamp = calendar.timegm(self.utc.timetuple())
+        struct_tmL = time.localtime(self.unix_stamp)
+        NOZStr = time.strftime(ISO_DATE_FORMAT_NOZ, struct_tmL)
+        # Don't reference time.altzone unless time.daylight is set.
+        localSecondsOffset = time.timezone
+        if (time.daylight and struct_tmL.tm_isdst):
+            localSecondsOffset = time.altzone
+
+        # localSecondsOffset's sign is reversed..
+        sign = "+" 
+        if (localSecondsOffset>0): sign = "-"
+        hourOffset = int(localSecondsOffset)/3600
+        minuteOffset = (int(localSecondsOffset) % 3600)/60
+        return "%s%s%02d%02d" % (NOZStr,sign,hourOffset,minuteOffset)
+
+def test_construct():
+    return iso8601(TESTDATE)
+
 def test_noop():
     pass
 
@@ -113,16 +170,18 @@ if __name__ == "__main__":
     # os.environ['TZ'] = 'AEST-10AEDT-11,M10.5.0,M3.5.0'
 
     
-    print " strptime: %s" % test_strptime()
-    print "   regexp: %s" % test_regexp()
-    print "  tpl-iso: %s" % test_tupletoiso8601()
-    print "  local-p: %s" % test_localfromstrp()
-    print " local-re: %s" % test_localre()
+    #print " strptime: %s" % test_strptime()
+    #print "   regexp: %s" % test_regexp()
+    #print "  tpl-iso: %s" % test_tupletoiso8601()
+    #print "  local-p: %s" % test_localfromstrp()
+    #print " local-re: %s" % test_localre()
+    print "  constr: %s" % test_construct()
 
     number=1000
     repeat=1
     from timeit import Timer
-    for tname in ['noop','strlen','strptime','regexp','tupletoiso8601','localfromstrp','localre']:
+    #for tname in ['noop','strlen','strptime','regexp','tupletoiso8601','localfromstrp','localre']:
+    for tname in ['noop','regexp','tupletoiso8601','localre','construct']:
         t = Timer("test_%s()"%tname, "from __main__ import test_%s"%tname)
         results = t.repeat(repeat=repeat,number=number)
         for r in results:
