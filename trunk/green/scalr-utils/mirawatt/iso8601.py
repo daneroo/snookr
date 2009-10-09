0b36f3602e3dc71a10a0381d9026ca66ad83b214
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2009-10-09 19:54:55 +0000

    update iso with tzinfo stuff

--- trunk/green/scalr-utils/mirawatt/iso8601.py
+++ trunk/green/scalr-utils/mirawatt/iso8601.py
@@ -25,16 +25,16 @@
 #  IDEA use only naive datetime objects, or FixedOffset
 # but find normalized localtime representation
 # we can use time.localtime vs time.gmtime to do this.
-
 import datetime
 import time # strptime not in datetime before 2.5
+import tzinfo # mine
 import calendar
 import re
 import os
 import math
-import sys
 
 TESTDATE=    '2009-10-08T01:04:53Z'
+TESTDATE=    '2009-10-08T01:04:53-0400'
 TESTDATEFRAC='2009-10-08T01:04:53.456Z'
 ISO_DATE_FORMAT_Z =   '%Y-%m-%dT%H:%M:%SZ'
 ISO_DATE_FORMAT_NOZ = '%Y-%m-%dT%H:%M:%S'
@@ -51,7 +51,7 @@ class iso8601:
         - local: datetime.datetime tuple for local timezone
     """
     def __init__(self,stampStr):    
-        self.utc = self.parseAndSetUTC(stampStr)
+        self.utc = self.parse(stampStr)
         self.setTimestamp()
         self.setLocalAndSecondsOffset()
         self.lcl = "ZZ"#self.getLocalStr()
@@ -76,20 +76,27 @@ class iso8601:
     TZ_RE = (r"(?P<tzname>(Z|"
              r"(?P<tzhour>[+-][0-9]{2})((:?)(?P<tzmin>[0-9]{2}))?))")
 
-    pattern = re.compile(DATE_RE+TIME_RE+TZ_RE)
+    isopattern = re.compile(DATE_RE+TIME_RE+TZ_RE)
 
-    def parseAndSetUTC(self,stampStr):
+    def parse(self,stampStr):
         '''
         Parse an iso 8601 string and return a naive datetime object
         representing the UTC tuple for the passed datetime representation
         TODO: parse seperators,timezone and manipulate datetime tuple
         '''
-        match = pattern.match(stampStr)
+        match = self.isopattern.match(stampStr)
         if (match):
             g = match.groupdict()
+            print g
             # seconds rounded to int even thoug RE allows fractional seconds
             tuple_date = datetime.datetime(int(g['year']), int(g['month']), int(g['day']), int(g['hour']), int(g['minute']), int(float(g['second'])))
             # TODO add offset, use timedelta...
+            tuple_date = tuple_date.replace(tzinfo=tzinfo.FixedOffset(int(g['tzhour'] or 0),int(g['tzmin'] or 0)))
+            #secondsOffset = int(g['tzhour'] or 0)*3600 + int(g['tzmin'] or 0)*60
+            #timestamp = calendar.timegm(tuple_date.timetuple())
+            #nu_tm = time.gmtime(timestamp-secondsOffset)
+            #print "%s %d %d %s" % (tuple_date,secondsOffset,timestamp,nu_tm)
+            print "%s : %s" % (tuple_date,tuple_date.tzinfo)
             return tuple_date
         else:
             return None
@@ -162,18 +169,8 @@ def test_localre():
     return tuple_to_iso8601local(tuple6)
     pass
 
-# use raw strings r"..": which do not escape \'s
-pattern = re.compile(r"(?P<year>[0-9]{4})-"
-                     r"(?P<month>[0-9]{2})-"
-                     r"(?P<day>[0-9]{2})"
-                     r"T?"
-                     r"(?P<hour>[0-9]{2}):"
-                     r"(?P<minute>[0-9]{2}):"
-                     r"(?P<second>[0-9]{2}([,.][0-9]+)?)"
-                     r"Z"
-                     )
 def test_regexp():
-    match = pattern.match(TESTDATE)
+    match = iso8601.isopattern.match(TESTDATE)
     if (match):
         groups = match.groupdict()
         return groups
@@ -193,18 +190,19 @@ if __name__ == "__main__":
         parseTest('2009-10-08T010453Z')
         parseTest('2009-10-08T010453-0400')
         parseTest('2009-10-08T010453-04')
-        sys.exit()
-    print "Testing iso8601 Datetimes"
-    print "time module timezones:"
-    print "time.tzname: %s" % ('|'.join(time.tzname))
-    print "time.timezone: %s" % (time.timezone)
-    print "time.altzone: %s" % (time.altzone)
-    print "time.daylight: %s" % (time.daylight)
-    # timemodule's C implementation is based on localtime syscall
-    #print "os.environ['TZ']: %s" % (os.environ['TZ'])
-    # os.environ['TZ'] = 'US/Eastern'
-    # os.environ['TZ'] = 'EST+05EDT,M4.1.0,M10.5.0'
-    # os.environ['TZ'] = 'AEST-10AEDT-11,M10.5.0,M3.5.0'
+
+    if (False):
+        print "Testing iso8601 Datetimes"
+        print "time module timezones:"
+        print "time.tzname: %s" % ('|'.join(time.tzname))
+        print "time.timezone: %s" % (time.timezone)
+        print "time.altzone: %s" % (time.altzone)
+        print "time.daylight: %s" % (time.daylight)
+        # timemodule's C implementation is based on localtime syscall
+        #print "os.environ['TZ']: %s" % (os.environ['TZ'])
+        # os.environ['TZ'] = 'US/Eastern'
+        # os.environ['TZ'] = 'EST+05EDT,M4.1.0,M10.5.0'
+        # os.environ['TZ'] = 'AEST-10AEDT-11,M10.5.0,M3.5.0'
 
     
     #print " strptime: %s" % test_strptime()
