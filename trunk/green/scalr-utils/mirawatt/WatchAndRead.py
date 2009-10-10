8e70a11ace0d28819819ba623ab292c63d50ce84
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2009-10-10 18:32:55 +0000

    start handling lines and drift

--- trunk/green/scalr-utils/mirawatt/WatchAndRead.py
+++ trunk/green/scalr-utils/mirawatt/WatchAndRead.py
@@ -16,13 +16,17 @@
 #  
 # so to do that can we iterate over a changing vector ?
 
-import os
-import fileinput
 import time
-import hashlib
-import pickle
 
-def findPrefixedLogs(path,prefix='CC',includeCompressed=False):
+import fileinput
+import hashlib
+import iso8601
+import os
+import pickle
+import re
+from xml.dom import minidom
+
+def findPrefixedLogs(path, prefix='CC', includeCompressed=False):
     """
     Finds files (recursively) in the directory tree starting at 'path'
     who's basenames start with prefix
@@ -32,14 +36,14 @@ def findPrefixedLogs(path,prefix='CC',includeCompressed=False):
 
     Returns a sequence of paths for files found.
     """
-    matcherList = [ (lambda s: os.path.basename(s).startswith(prefix) ) ]
+    matcherList = [(lambda s: os.path.basename(s).startswith(prefix))]
     if includeCompressed:
-        matcherList.append( (lambda s: s.endswith('.log') or s.endswith('.log.bz2') or s.endswith('.log.gz')) )
+        matcherList.append((lambda s: s.endswith('.log') or s.endswith('.log.bz2') or s.endswith('.log.gz')))
     else:
-        matcherList.append( (lambda s: s.endswith('.log')) )
+        matcherList.append((lambda s: s.endswith('.log')))
 
-    fileList =  ffind(path,namefs=matcherList,relative=False)
-    fileList.sort(key=(lambda s: os.path.basename(s) ))
+    fileList = ffind(path, namefs=matcherList, relative=False)
+    fileList.sort(key=(lambda s: os.path.basename(s)))
     return fileList
 
 # From: http://muharem.wordpress.com/2007/05/18/python-find-files-and-search-inside-them-find-grep/
@@ -66,22 +70,22 @@ def ffind(path, namefs=None, relative=True):
     return(fileList)
 
 # return a copy of logFileList, which need processing according to progress
-def removeUnchanged(logFileList,progress): # Remove items which are unchanged in progress.
-    activeFileList=[];
+def removeUnchanged(logFileList, progress): # Remove items which are unchanged in progress.
+    activeFileList = [];
     for filename in  logFileList:
         newPI = ProgressItem(filename)
-        localtimeModStr =  time.strftime('%Y-%m-%dT%H:%M:%S',time.localtime(newPI.lastMod))
+        localtimeModStr = time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(newPI.lastMod))
         ago = time.time()-newPI.lastMod
         #print " checking: %s  len:%08d ago:%ds. mod:%s md5:%s" % (os.path.basename(newPI.fileName),newPI.fileSize,ago,localtimeModStr,newPI.md5)
-        oldPI=None
+        oldPI = None
         if (filename in progress):
-            oldPI=progress[filename]
+            oldPI = progress[filename]
         if (not oldPI): # new file: seed progress, calc md5, appent to active file list
             newPI.calcmd5()
             progress[filename] = newPI
             activeFileList.append(filename)
-            print " NEW: %s  len:%08d ago:%ds. mod:%s md5:%s" % (os.path.basename(newPI.fileName),newPI.fileSize,ago,localtimeModStr,newPI.md5)
-        elif (newPI.fileSize!=oldPI.fileSize or newPI.lastMod!=oldPI.lastMod):
+            print " NEW: %s  len:%08d ago:%ds. mod:%s md5:%s" % (os.path.basename(newPI.fileName), newPI.fileSize, ago, localtimeModStr, newPI.md5)
+        elif (newPI.fileSize != oldPI.fileSize or newPI.lastMod != oldPI.lastMod):
             # current file is in progress, but size/date has changed
             # update info in progress, add to active List (calc md5)
             # make sure we preserve le lastLineRead (high water mark)
@@ -90,7 +94,7 @@ def removeUnchanged(logFileList,progress): # Remove items which are unchanged in
             # replace with new progress item
             progress[filename] = newPI
             activeFileList.append(filename)
-            print " MOD: %s  len:%08d ago:%ds. mod:%s md5:%s" % (os.path.basename(newPI.fileName),newPI.fileSize,ago,localtimeModStr,newPI.md5)
+            print " MOD: %s  len:%08d ago:%ds. mod:%s md5:%s" % (os.path.basename(newPI.fileName), newPI.fileSize, ago, localtimeModStr, newPI.md5)
         else:
             #print " SKP: %s  len:%08d ago:%ds. mod:%s md5:%s" % (os.path.basename(oldPI.fileName),oldPI.fileSize,ago,localtimeModStr,oldPI.md5)
             pass
@@ -98,8 +102,8 @@ def removeUnchanged(logFileList,progress): # Remove items which are unchanged in
     return activeFileList
 
 def md5OfFileContent(fileName): # if bz2 or gz md5 of uncompressed content
-    mode='r'
-    fp = fileinput.hook_compressed(fileName,mode)
+    mode = 'r'
+    fp = fileinput.hook_compressed(fileName, mode)
     md5 = hashlib.md5()
     try:
         while 1:
@@ -112,7 +116,7 @@ def md5OfFileContent(fileName): # if bz2 or gz md5 of uncompressed content
     return md5.hexdigest()
 
 class ProgressItem:
-    def __init__(self,fileName):
+    def __init__(self, fileName):
         self.fileName = fileName
         stats = os.stat(fileName)
         self.fileSize = stats[6]
@@ -123,7 +127,7 @@ class ProgressItem:
     def calcmd5(self):
         self.md5 = md5OfFileContent(self.fileName)
 
-progressPickleFileName='progress.pkl'
+progressPickleFileName = 'progress.pkl'
 def loadProgressFromPickle():
     progress = {} # map of file -> ProgressItem
     if (os.path.exists(progressPickleFileName)):
@@ -141,37 +145,82 @@ def saveProgressToPickle(progress):
 
 def onepass(progress):
     # renew the list
-    logFileList = findPrefixedLogs(os.curdir,prefix='CC2',includeCompressed=True)
-    activeFileList = removeUnchanged(logFileList,progress)
-    if (len(activeFileList)<=0):
+    logFileList = findPrefixedLogs(os.curdir, prefix='CC2', includeCompressed=True)
+    activeFileList = removeUnchanged(logFileList, progress)
+    if (len(activeFileList) <= 0):
         print "No files to process"
         return
 
-    for line in fileinput.input(activeFileList,openhook=fileinput.hook_compressed):
-        if (fileinput.filelineno()<=progress[fileinput.filename()].lastLineRead):
+    for line in fileinput.input(activeFileList, openhook=fileinput.hook_compressed):
+        if (fileinput.filelineno() <= progress[fileinput.filename()].lastLineRead):
             #print "skip  %06d from:%s" % (fileinput.filelineno(),fileinput.filename())
             continue
 
         # indicate progress (before or after actual processing ?
-        progress[fileinput.filename()].lastLineRead=fileinput.filelineno()
+        progress[fileinput.filename()].lastLineRead = fileinput.filelineno()
 
         # actually process the line
-        #process(line)
+        handleLine(line)
 
-        if (fileinput.filelineno()%1000==0):
+        if (fileinput.filelineno() % 1000 == 0):
             pass #print "file:%s:%06d  -mod:%s" % (os.path.basename(fileinput.filename()),fileinput.filelineno(),lastModDate)
         #print "file:%s:%06d" % (os.path.basename(fileinput.filename()),fileinput.filelineno())
 
     saveProgressToPickle(progress)
     print "Processing summary:"
-    for fileName in sorted(progress.keys(),key=(lambda s: os.path.basename(s)) ):
-        print " %7d lines from %s in %s" % (progress[fileName].lastLineRead,os.path.basename(fileName),os.path.dirname(fileName))
+    for fileName in sorted(progress.keys(), key=(lambda s: os.path.basename(s))):
+        print " %7d lines from %s in %s" % (progress[fileName].lastLineRead, os.path.basename(fileName), os.path.dirname(fileName))
+
+MARKStamp = None #'2000-01-01T00:00:00'
+MARKCheckLen = 13      # 13 for hour,16 for minute
+def handleLine(line):
+    if (line[:4] == "<!--"):
+        return
+    stampStr = line[:24]
+    CCStr = line[25:-1] # to remove the newline ?
+    # omit empty lines
+
+    # Mark the logs as the stampStr advances.
+    global MARKStamp
+    if (stampStr[:MARKCheckLen] != MARKStamp):
+        print "# MARK -- %s" % stampStr
+    MARKStamp = stampStr[:MARKCheckLen]
+
+    if (len(CCStr) > 0):
+        parseFragment(stampStr, CCStr)
+
+def parseFragment(stampStr, ccfragment):
+    # date format: 2009-07-02T19:08:12-0400
+    isodt = iso8601.parse_iso8601(stampStr)
+
+    try:
+        pass #ccdom = minidom.parseString(ccfragment)
+    except Exception, e:
+        print "XML Error: %s : %s" % (stampStr, e)
+        return
+
+    #calculate drift
+    ccTimeStr = '00:00:00' #ccdom.getElementsByTagName('time')[0].childNodes[0].nodeValue
+    matchTime = re.search("<time>(?P<time>[0-9:]{8})</time>", ccfragment)
+    if (matchTime):
+        ccTimeStr = matchTime.group('time')
+    ccStampStr = stampStr[:-13] + ccTimeStr + stampStr[-5:]
+    #print "stamp: %s cc:%s" %(isodt,ccStampStr)
+    isoCC = iso8601.parse_iso8601(ccStampStr)
+    delta = isoCC - isodt
+    drift = delta.days * 86400 + delta.seconds #+delta.microseconds/1000000.0
+    if (drift > 43200):
+        drift = -86400 + drift
+    elif (drift < -43200):
+        drift = 86400 + drift
+    if (abs(drift) > 600):
+        print "WARNING clock drift: %f seconds " % (drift)
 
 if __name__ == "__main__":
     print "Prefixed File Logs (can be compressed)"
 
-    progress = loadProgressFromPickle() # map of file -> ProgressItem
-    for n in range(1,400):
+    progress = {} # loadProgressFromPickle() # map of file -> ProgressItem
+    for n in range(1, 2):
         print "Processing loop: %d" % n
         onepass(progress)
         time.sleep(5.0)
