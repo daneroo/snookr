19fb1bc1df60107c7824a9e0ee0730f4dd4cfce6
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2009-10-13 00:02:44 +0000

    -s silent, -S show errer

--- trunk/green/scalr-utils/mirawatt/wdog/publish.sh
+++ trunk/green/scalr-utils/mirawatt/wdog/publish.sh
@@ -4,7 +4,10 @@
 USERNAME="jean"
 FEEDFILE="/mirawatt/feeds.xml"
 
+# -s: silent  : disble progress meter and errors
+# -S : show error, when used with -s, re-enables error logging
+
 while true; do  
-    curl -m 30 -F "owner=${USERNAME}" -F "content=@${FEEDFILE};type=text/xml"  http://imetrical.appspot.com/post; 
+    curl -s -S -m 30 -F "owner=${USERNAME}" -F "content=@${FEEDFILE};type=text/xml"  http://imetrical.appspot.com/post; 
     sleep 60;
 done
