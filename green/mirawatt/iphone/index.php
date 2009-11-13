9946646bb7ac805e630a1ea628d07b547ebc04d4
Daniel Lauzon <daneroo@users.noreply.github.com>
2009-11-13 22:12:22 +0000

    start detecting

--- green/mirawatt/iphone/index.php
+++ green/mirawatt/iphone/index.php
@@ -1,3 +1,10 @@
+Mirawatt - ted - iphone Landing page
+browser detection: a la
+<pre>
+if(!((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)))) {
+                    imIsIPhone=false;
+}
+</pre>
 <?
 phpinfo();
 ?>
