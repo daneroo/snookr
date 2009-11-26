fb19307173efea57751d1f42b297c07e9fa32f73
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2009-11-26 19:54:34 +0000

    demo page for ted

--- trunk/green/mirawatt/iphone/index.php
+++ trunk/green/mirawatt/iphone/index.php
@@ -1,10 +1,69 @@
-Mirawatt - ted - iphone Landing page
-browser detection: a la
-<pre>
-if(!((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)))) {
-                    imIsIPhone=false;
-}
-</pre>
-<?
-phpinfo();
-?>
+<!doctype html>
+<html manifest="mirawatt-iphone.manifest.php">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Mirawatt - for TED5000</title>
+        <link rel="stylesheet" href="mirawatt-wp-style.css" type="text/css" media="screen">
+        <script type="text/javascript" charset="utf-8">
+            if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+                window.location = "iphone-ted.html";
+            }
+        </script>
+        <style type="text/css" media="screen">
+        </style>
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <div id="logo">
+                    <a href="http://ted.mirawatt.com/">
+                        <img style="margin-top: -15px;" src="mirawatt-wp-pixel-logo-400x75-trans.png">
+                    </a>
+                </div>
+                <div class="cleared"></div>
+            </div> <!-- Closes header -->
+
+            <div id="catnav">
+                <div id="toprss"></div> <!-- Closes toprss -->
+                <ul id="nav">
+                    <li><a href="http://ted.mirawatt.com/">Home</a></li>
+                    <li><a href="http://ted.mirawatt.com/">Simulation</a></li>
+                </ul>
+            </div> <!-- Closes catnav -->
+
+            <div class="cleared"></div>
+
+            <div id="main">
+                <div id="contentwrapper">
+                    <div class="topPost">
+                        <h2 class="topTitle"><a href="http://ted.mirawatt.com/">Mirawatt - for TED</a></h2>
+
+                        <div class="topContent">
+                            <p>Intructions: to add this an iPhoe web app, </p>
+                            <ul>
+                                <li>Navigate to http://ted.mirawatt.com/</li>
+                            </ul>
+
+                            <div style="position:relative; margin-left:400px; margin-top:-240px; width:300px;">
+                            <div style="width:408px; height:740px; background-image:url('mirawattIPhone-bg-for-320x460.png'); background-repeat:none; background-position:0 0; padding:0px; margin:0px; ">
+                            <div style="padding-left:45px;padding-top:157px;">
+                            <div style="width:320px; height:460px; overflow:hidden;">
+                            <!-- height to accomodate info section on chrome... scroll=no has no effect --><br />
+                            <iframe src ="http://ted.mirawatt.com/iphone-ted.html" width="320px"  height="560px" frameborder="0" scrolling="no" allowTransparency="true"></p>
+                                <p>Your browser does not support iframes.</p>
+                            </iframe>
+
+                            </div>
+                            </div>
+                            </div>
+                            </div>
+
+                        </div> <!-- close topContent -->
+
+                        <div class="cleared"></div>
+                    </div> <!-- Closes topPost -->
+                </div> <!-- Closes contentwrapper-->
+            </div><!-- Closes Main -->
+        </div><!-- Closes wrapper -->
+    </body>
+</html>
