a0ed87cbf6f4ee55c7aec8112dc444724f5b488f
Daniel Lauzon <daneroo@users.noreply.github.com>
2009-12-22 17:50:26 +0000

    mirawatt ted5k init

---
+++ xcode/iphone/Mirawatt-ted5k/Classes/Mirawatt_ted5kViewController.h
@@ -0,0 +1,18 @@
+//
+//  Mirawatt_ted5kViewController.h
+//  Mirawatt-ted5k
+//
+//  Created by Daniel Lauzon on 22/12/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Mirawatt_ted5kViewController : UIViewController {
+	UIWebView *webView;
+}
+
+@property (nonatomic, retain) IBOutlet UIWebView *webView;
+
+@end
+
