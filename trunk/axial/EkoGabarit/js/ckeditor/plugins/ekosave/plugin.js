585a522d341a03bec0e5fde0874c221a6cfcf2b4
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2010-03-17 15:50:04 +0000

    EkoGabarit with floating in-place editor!

---
+++ trunk/axial/EkoGabarit/js/ckeditor/plugins/ekosave/plugin.js
@@ -0,0 +1,50 @@
+﻿/*
+ *I adapted this from instructions at:
+ * http://stackoverflow.com/questions/1956967/ckeditor-ajax-save
+ *
+*/
+
+/**
+ * @axialsave plugin.
+ */
+
+(function()
+{
+    var saveCmd =
+    {
+        modes : {
+            wysiwyg:1,
+            source:1
+        },
+
+        exec : function( editor )
+        {
+            // not sure what this does.
+            editor.updateElement();
+            //alert('called axialsave');
+            if (ekoSaveCallback) {
+                ekoSaveCallback(editor);
+            }
+        }
+    };
+
+    var pluginName = 'ekosave';
+
+    // Register a plugin named "save".
+    CKEDITOR.plugins.add( pluginName,
+    {
+        init : function( editor )
+        {
+            var command = editor.addCommand( pluginName, saveCmd );
+            //command.modes = { wysiwyg : !!( editor.element.$.form ) };
+
+            editor.ui.addButton( 'ekosave',
+            {
+                label : editor.lang.save,
+                icon: 'skins/kama/icons.png',
+                iconOffset: 2,
+                command : pluginName
+            });
+        }
+    });
+})();
