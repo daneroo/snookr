6d0f428f8787b242c4a763920747d0cb62ab82b8
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2010-03-18 16:38:29 +0000

    discard dialogs

--- trunk/axial/EkoGabarit/ekolib-gabarit-1.0.0.js
+++ trunk/axial/EkoGabarit/ekolib-gabarit-1.0.0.js
@@ -9,25 +9,96 @@ EkoGabarit.prototype = {
     // Theese are the three jQ element hooks.
     currentEditingElt:null,
     dialogElt:null,
-    ckElt:null,
+    ckElt:null,    // j\the jQ Object that the CKEditor was replaced into
+    ckeditor:null, // the CKEditor object itself
     debugelt:null,
     debug:function(message){
         if (this.debugelt) {
             $(this.debugelt).text('DBG:'+' '+message);
         }
     },
-    render: function(){
+    render: function (divselector){
+        this.ckElt = $('<textarea class="dlg_ckeditor" cols="80" id="editor1" name="editor1" rows="10">CONTENT</textarea>');
+        this.dialogElt = $('<div style="position:absolute;">').append(this.ckElt);
+        $(divselector).append(this.dialogElt);
+        this.dialogElt.resizable().draggable();
+        this.dialogElt.hide();
+
+        this.confirmElt = $('<div id="dialog-confirm" title="Save changes ?">You have unsaved edits in this text!</div>');
+        $(divselector).append(this.confirmElt);
+        var ekoG = this; // alias for this in callbacks!
+        this.confirmElt.dialog({
+            resizable: false,
+            //height:180,
+            modal: true,
+            autoOpen: false,
+            buttons: {
+                'Cancel': function() {
+                    $(this).dialog('close');
+                },
+                'Save': function() {
+                    $(this).dialog('close');
+                    ekoG.save();
+                },
+                'Discard': function() {
+                    $(this).dialog('close');
+                    var invokeDialogIfDirty = false
+                    ekoG.discard(invokeDialogIfDirty);
+                }
+            }
+        });
+
+        var ck_editor_height=300;
+        var ck_config = {
+            BSCtoolbar:'Basic',
+            toolbar: [
+            ['ekodiscard', 'ekosave','Preview' ,'-', 'Bold', 'Italic' ]
+            ],
+            extraPlugins:'ekosave,ekodiscard',
+            ZZtoolbar: [
+            ['Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink'],
+            ['UIColor']
+            ],
+            resize_enabled : false,
+            height: ck_editor_height
+        };
+
+        var ekoG = this; // alias for this in callbacks!
+        var ck_initCallback = function(){
+            ekoG.ckeditor = ekoG.ckElt.ckeditorGet();
+            ekoG.ckeditor.ekoGabarit = ekoG;
+        }
+        this.ckElt.ckeditor(ck_initCallback,ck_config);
 
     },
     save: function(){
-        var ekoG=this;
+        var ekoG = this; // alias for this in callbacks!
         if(ekoG.currentEditingElt){
-            //alert(editor.getData());
             ekoG.currentEditingElt.html(ekoG.ckElt.val());
-            ekoG.currentEditingElt.show();
+            ekoG.currentEditingElt = null;
             ekoG.dialogElt.hide();
         }
-        // add an ajax/update hook here...
+    // add an ajax/update hook here...
+    },
+    discardWithDialog: function(){ // willcause save or discard on dialog click!
+        if (this.isDirty()) {
+            this.confirmElt.dialog('open');
+        } else {
+            this.discard();
+        }
+    },
+    discard: function(){ // juste discard, no dialog
+        if(this.currentEditingElt){
+            this.currentEditingElt = null;
+        }
+        this.dialogElt.hide();
+    },
+    isDirty: function() {
+        // cannot count on ckeditor.checkDirty()!
+        if (!this.currentEditingElt) {
+            return false;
+        }
+        return this.currentEditingElt.html()!=this.ckElt.val();
     },
     inject:function(){
         var ekoG = this; // alias for this in callbacks!
@@ -41,14 +112,16 @@ EkoGabarit.prototype = {
                 $(this).removeClass("editablehover");
             });
             $(this).dblclick(function(){
+                //ekoG.debug($.toJSON(ekoG.ckeditor.checkDirty()));
+                if (ekoG.isDirty()){
+                    ekoG.confirmElt.dialog('open');
+                    return;
+                }
                 ekoG.currentEditingElt = $(this);
                 var celtoff = ekoG.currentEditingElt.offset();
                 var celtwidth = ekoG.currentEditingElt.width();
-                //alert('pos: '+$.toJSON(celtoff)+' '+ekoG.currentEditingElt.width()+'x'+ekoG.currentEditingElt.height());
 
                 ekoG.ckElt.val( ekoG.currentEditingElt.html() );
-                //ekoG.currentEditingElt.hide();
-                //ekoG.dialogElt.insertAfter(ekoG.currentEditingElt);
                 ekoG.dialogElt.show();
                 var dialogEltOffset = ekoG.dialogElt.offset();
                 var contentElt = ekoG.dialogElt.find('.cke_contents');
@@ -66,6 +139,9 @@ EkoGabarit.prototype = {
                 var nuwidth = celtwidth+compensate.width;
                 if (nuwidth<200) nuwidth=200;
                 ekoG.dialogElt.width(nuwidth);
+
+                ekoG.ckeditor.resetDirty();
+                ekoG.debug('XXX:'+$.toJSON(ekoG.ckeditor.checkDirty()));
             });
         });
     },
@@ -92,13 +168,13 @@ EkoGabarit.prototype = {
     genOid: function(){ // static methid! can be used for class-wide (static) counter
         EkoGabarit.prototype.gabaritOid+=1;
         return EkoGabarit.prototype.gabaritOid;
-    }
-};
+        }
+        };
 
 
-function EkoGabarit(previewSelector) {
-    this.setOid();
-    this.previewJQ = $(previewSelector);
+        function EkoGabarit(previewSelector) {
+        this.setOid();
+        this.previewJQ = $(previewSelector);
 }
 
 
---
+++ trunk/axial/EkoGabarit/js/ckeditor/plugins/ekodiscard/plugin.js
@@ -0,0 +1,52 @@
+﻿/*
+ *I adapted this from instructions at:
+ * http://stackoverflow.com/questions/1956967/ckeditor-ajax-save
+ *
+*/
+
+/**
+ * @axialsave plugin.
+ */
+
+(function()
+{
+    var discardCmd =
+    {
+        modes : {
+            wysiwyg:1,
+            source:1
+        },
+
+        exec : function( editor )
+        {
+            // not sure what this does.
+            editor.updateElement();
+            //alert('called axialsave');
+
+            var ekoG = editor.ekoGabarit;
+            if(!ekoG) return;
+            if(!ekoG.discard) return;
+            ekoG.discardWithDialog();
+        }
+    };
+
+    var pluginName = 'ekodiscard';
+
+    // Register a plugin named "save".
+    CKEDITOR.plugins.add( pluginName,
+    {
+        init : function( editor )
+        {
+            var command = editor.addCommand( pluginName, discardCmd );
+            //command.modes = { wysiwyg : !!( editor.element.$.form ) };
+
+            editor.ui.addButton( 'ekodiscard',
+            {
+                label : 'Discard',
+                icon: 'skins/kama/icons.png',
+                iconOffset: 13,
+                command : pluginName
+            });
+        }
+    });
+})();
