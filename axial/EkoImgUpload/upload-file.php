92509c494975d94e3239d535268174d9c38876a1
Daniel Lauzon <daneroo@users.noreply.github.com>
2010-03-22 17:55:39 +0000

    update ajaxupload version

--- axial/EkoImgUpload/upload-file.php
+++ axial/EkoImgUpload/upload-file.php
@@ -1,4 +1,6 @@
 <?php
+// Change this to allow <yourdomain.com> to make it accessible to your site, or allow <*> for ANYONE to be able to access it.
+header('Access-Control: allow <*>');
 $uploaddir = './uploads/'; 
 $file = $uploaddir . basename($_FILES['uploadfile']['name']); 
  
