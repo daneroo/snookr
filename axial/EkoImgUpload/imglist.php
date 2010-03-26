c7ab078cb48c27a28015697abd2d4539d17333b5
Daniel Lauzon <daneroo@users.noreply.github.com>
2010-03-26 18:11:00 +0000

    eko image picker - refactor and simplify

--- axial/EkoImgUpload/imglist.php
+++ axial/EkoImgUpload/imglist.php
@@ -2,12 +2,15 @@
 header('Content-type: text/javascript');
 $rex = "/uploads\/.*\.$i(jpg|jpeg|png|gif)/i";
 $imglist = array();
+$baseURL="http://axial.imetrical.com/EkoImgUpload/";
 foreach (glob("uploads/*.*") as $filename) {
     if ($rex!="" && !preg_match($rex, $filename)) {
         //echo "REMOVE $filename size " . filesize($filename) . "<br />\n";
         continue;
     }
-    array_push($imglist, $filename);
+    $url = $baseURL . $filename;
+    $imgStruct = array ('url'=>$url, 'thumb'=>$url."#thumb", 'name'=>basename($filename) );
+    array_push($imglist, $imgStruct);
 }
 //print_r($imglist);
 if (false) foreach ($imglist as $filename) {
