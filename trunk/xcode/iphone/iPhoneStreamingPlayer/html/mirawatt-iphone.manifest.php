52fa1fc03f9129a3ed0af8ba68eb3500cf69601d
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2010-04-28 21:15:29 +0000

    start

---
+++ trunk/xcode/iphone/iPhoneStreamingPlayer/html/mirawatt-iphone.manifest.php
@@ -0,0 +1,66 @@
+<?php 
+header("Content-Type: text/cache-manifest");
+?>CACHE MANIFEST
+# stamp: 200909290029
+# javascript
+charturl.js
+fetchted5k.js
+jquery.1.3.2.min.js
+mirawatt-1.0.js
+imetrical-1.0.js
+charturl.js
+googleviz/jsapi-1.0.js
+googleviz/googleviz-1.0.js
+jqtouch/jqtouch.js
+jqtouch/jqtouch.min.js
+jqtouch/jquery.1.3.2.min.js
+jquery.timer.js
+# css
+jqtouch/jqtouch.css
+jqtouch/jqtouch.min.css
+themes/apple/theme.css
+themes/apple/theme.min.css
+themes/jqt/theme.css
+themes/jqt/theme.min.css
+# images
+mirawattIPhoneAbout-200x200.png
+mirawattIPhoneIcon-57x57.png
+mirawattIPhoneStartup-320-460.png
+themes/apple/img/backButton.png
+themes/apple/img/blueButton.png
+themes/apple/img/cancel.png
+themes/apple/img/chevron.png
+themes/apple/img/grayButton.png
+themes/apple/img/listArrowSel.png
+themes/apple/img/listGroup.png
+themes/apple/img/on_off.png
+themes/apple/img/pinstripes.png
+themes/apple/img/selection.png
+themes/apple/img/thumb.png
+themes/apple/img/toggle.png
+themes/apple/img/toggleOn.png
+themes/apple/img/toolbar.png
+themes/apple/img/toolButton.png
+themes/apple/img/whiteButton.png
+themes/jqt/img/back_button.png
+themes/jqt/img/back_button_clicked.png
+themes/jqt/img/button.png
+themes/jqt/img/button_clicked.png
+themes/jqt/img/chevron.png
+themes/jqt/img/chevron_circle.png
+themes/jqt/img/grayButton.png
+themes/jqt/img/on_off.png
+themes/jqt/img/rowhead.png
+themes/jqt/img/toggle.png
+themes/jqt/img/toggleOn.png
+themes/jqt/img/toolbar.png
+themes/jqt/img/whiteButton.png
+
+NETWORK:
+staticfeed.xml
+/feeds
+http://chart.apis.google.com/chart
+# seems prefix is sufficient...more stable!
+#http://www.google.com/
+#http://www.google.com/jsapi
+#http://www.google.com/uds/
