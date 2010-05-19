d6937e9d8e415214beb70364791cb58de49d079d
daniel.lauzon <daniel.lauzon@40fcb585-e637-0410-b9f2-59221c3c02b4>
2010-05-19 16:28:40 +0000

    un-ignore build

---
+++ trunk/axial/ekolib/build/jslint-check.js
@@ -0,0 +1,63 @@
+load("build/jslint.js");
+
+
+var eachSource=true;
+if (!eachSource) {    
+    doOne("dist/ekolib.js");
+} else {
+    srcs = [
+        "src/ekolib-core-1.0.0.js",
+        "src/ekolib-constants-1.0.0.js",
+        "src/ekolib-data-1.0.0.js",
+        "src/ekolib-editor-1.0.0.js",
+        "src/ekolib-chained-1.0.0.js",
+        "src/ekolib-render-1.0.0.js",
+        "src/ekolib-lifecycle-1.0.0.js"
+        ];
+    for (var i=0;i<srcs.length;i++){
+        doOne(srcs[i]);
+    }
+}
+
+function doOne(srcFilename){
+    
+    var src = readFile(srcFilename);
+    JSLINT(src, {
+        evil: true,
+        forin: true
+    });
+
+    // All of the following are known issues that we think are 'ok'
+    // (in contradiction with JSLint) more information here:
+    // http://docs.jquery.com/JQuery_Core_Style_Guidelines
+    //
+    // Let's start from scratch'
+    var ok = {
+    /*
+	"Expected an identifier and instead saw 'undefined' (a reserved word).": true,
+	"Use '===' to compare with 'null'.": true,
+	"Use '!==' to compare with 'null'.": true,
+	"Expected an assignment or function call and instead saw an expression.": true,
+	"Expected a 'break' statement before 'case'.": true
+    */
+    };
+
+    var e = JSLINT.errors, found = 0, w;
+
+    for ( var i = 0; i < e.length; i++ ) {
+        w = e[i];
+
+        if ( !ok[ w.reason ] ) {
+            found++;
+            print( srcFilename+":" + w.line + " character " + w.character + ": " + w.reason );
+            print( w.evidence + "\n" );
+        }
+    }
+
+    if ( found > 0 ) {
+        print( "\n" + found + " Error(s) found." );
+
+    } else {
+        print( "JSLint check passed for "+srcFilename+".");
+    }
+}
